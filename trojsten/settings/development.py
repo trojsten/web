801f7ac1a3139b238571e22c5fbdc0e5065a0cb2
Michal Petrucha <michal.petrucha@ksp.sk>
2013-06-14 19:49:37 +0200

    Empty project template

---
+++ trojsten/settings/development.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from trojsten.settings.common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'database.sqlite3'),
+    },
+}
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+INSTALLED_APPS += ('debug_toolbar',)
+MIDDLEWARE_CLASSES = (('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    + MIDDLEWARE_CLASSES)
+
+INTERNAL_IPS = ('127.0.0.1',)
---
+++ trojsten/settings/production.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from trojsten.settings.common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'trojsten',
+        'USER': 'trojsten',
+    }
+}
+
+STATIC_ROOT = '/var/www/trojsten/static/'
+MEDIA_ROOT = '/var/www/trojsten/media/'
+MEDIA_URL = '/media/'
+
+try:
+    from trojsten.settings.local import *
+except ImportError:
+    pass
