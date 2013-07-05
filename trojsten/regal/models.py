6bc990100ce57d6b7d6d983c25b29c8da5e25749
Sysel <marian.sysel.hornak@gmail.com>
2013-07-06 01:58:41 +0200

    Vraciam sa k staremu regals/models.py, novy ma zatial neidentifikovanu chybu. Je dole zakomentovany.

--- trojsten/regal/models.py
+++ trojsten/regal/models.py
@@ -1,5 +1,71 @@
+# -*- coding: utf-8 -*- 
+
 from django.db import models
 from datetime import date
+from django.core.exceptions import ObjectDoesNotExist
+
+class Address(models.Model):
+    street = models.CharField(max_length=64)
+    number = models.CharField(max_length=16)
+    town = models.CharField(max_length=64)
+    postal_code = models.CharField(max_length=16)
+    country = models.CharField(max_length=32)
+    
+    def __unicode__(self):
+        return (
+            self.street + " " + self.number + ", " +
+            self.town + ", " + self.postal_code + ", " +
+            self.country)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    birth_date = models.DateField()
+    
+    def __unicode__(self):
+        return self.name + " " + self.surname
+
+        
+class School(models.Model):
+    name = models.CharField(max_length=128)
+    abbr = models.CharField(max_length=16, primary_key=True)
+        # used instead of id
+        # used when full name is too long (e.g. results, list of participants)
+        # note: primary_key=True implies null=False and unique=True
+    address = models.ForeignKey(Address, related_name='schools_here')
+    studying_persons = models.ManyToManyField(
+        Person,
+        through='Student',
+        related_name='studies_in')
+    teaching_persons = models.ManyToManyField(
+        Person,
+        through='Teacher',
+        related_name='teaches_in')
+    
+    def __unicode__(self):
+        return self.name
+    
+
+class Student(models.Model):
+    """ A relationship between School and Person.
+        
+    """
+    school = models.ForeignKey(School, related_name='students')
+    person = models.ForeignKey(Person, related_name='studies_as')
+    
+    
+
+class Teacher(models.Model):
+    """ A relationship between School and Person.
+    """
+    school = models.ForeignKey(School, related_name='teachers')
+    person = models.ForeignKey(Person, related_name='teaches_as')
+
+        
+
+
+''' NIECO MU VADI, FAKT NEVIEM CO:
 
 ##############################
 #    PERSONAL INFORMATIONS   #
@@ -41,7 +107,23 @@ class Person(models.Model):
     # teaches_as <- related name to Teacher.person
     # teaches_in <- related name to School.teaching_persons
     
-    # TODO(sysel) stuff to school / student management
+    def actual_student(self, act_date=None):
+        # TODO docstring
+        if act_date == None:
+            act_date = date.now()
+        try:
+            r = self.studies_as.get(end_date = None)
+            return r
+        except ObjectDoesNotExist:
+            return None
+         
+    def close_actual_student(self):
+        # TODO docstring
+        act_student = self.actual_student()
+        if act_student == None:
+            return
+        act_student.end_date = date.now()
+        act_student.save()
     
     def __unicode__(self):
         return self.name + " " + self.surname
@@ -82,49 +164,25 @@ class Student(models.Model):
     """
     school = models.ForeignKey(School, related_name='students')
     person = models.ForeignKey(Person, related_name='studies_as')
-    study_type = StudyTypeField()
-    start_date = models.DateField()
+    study_type = StudyTypeField(default = StudyType.ZS)
+    start_date = models.DateField(auto_now_add=True)
         # Begining date of this relationship
         # Used to determine actual school in specified time
-    end_date = models.DateField()
+    end_date = models.DateField(default=None, editable=False)
         # Termination date of this relationship
         # Used to determine actual school in specified time
-    expected_end_year = models.SmallIntegerField()
+        # None/NULL when this is actual relationship
+    expected_end_year = models.SmallIntegerField(default=None)
         # The year when full study would terimnate
         # Used to determine actual grade
     
     def __init__(self, *args, **kwargs):
-        if not "study_type" in kwargs:
-            kwargs["study_type"] = StudyType.SS
-        act_year = date_to_school_year(date.today())
-        study_lenght = kwargs["study_type"].max_grade
-        if not "start_date" in kwargs:
-            kwargs["start_date"] = date(act_year-1,9,1);
-        if not "end_date" in kwargs:
-            kwargs["start_date"] = date(act_year+study_lenght,8,31);
-        if not "expected_end_year" in kwargs:
-            kwargs["expected_end_year"] = act_year+study_lenght
         super(Student, self).__init__(self, *args, **kwargs)
+        if self.expected_end_year == None :
+            self.set_grade(1)
     
-    # Simple enum for validity
-    ACTUAL = 0
-    PAST = -1
-    FUTURE = 1
-    
-    def validity(self, act_date = None):
-        """ Returns whether this relationship is ACTUAL,
-            will be actual in the FUTURE, or was actual in the PAST
-            using hilighted constants of class Student.
-            You can simulate another date by optional argument "act_date".
-        """
-        if act_date == None:
-            act_date = date.now()
-        if act_date < self.start_date:
-            return Student.PAST
-        elif act_date <= self.end_date
-            return Student.ACTUAL
-        else
-            return Student.FUTURE
+    def is_actual():
+        return end_date == None
     
     def get_grade(self, act_date = None):
         """ Returns actual grade calculated from expected_end_year
@@ -144,41 +202,12 @@ class Student(models.Model):
         act_year = Student.date_to_school_year(act_date)
         self.expected_end_year = act_year + self.study_type.max_grade - grade
         
-    def auto_get_grade(self, act_date = None):
-        """ Returns actual grade in a inteligent way:
-            Before (after) active period returns starting (terminating) grade.
-            You can simulate another date by optional argument "act_date".
-        """
-        if act_date == None:
-            act_date = date.now()
-        return self.get_act_grade(nearest_active_date(act_date))
-        
-    def auto_set_grade(self, grade, act_date = None):
-        """ Sets actual grade in a inteligent way:
-            Before (after) active period sets starting (terminating) grade.
-            You can simulate another date by optional argument "act_date".
-        """
-        if act_date == None:
-            act_date = date.now()
-        self.set_act_grade(grade, nearest_active_date(act_date))
-    
-    def nearest_active_date(self, act_date):
-        """ Returns date from active period of this relationship
-            which is nearest to given date.
-        """
-        if act_date < self.start_date:
-            return self.start_date 
-        elif act_date > self.end_date:
-            return self.end_date
-        else
-            return act_date
-            
     @staticmethod
     def date_to_school_year(date_):
         """ Returns a second year of a school year of given date. """
         if date_.month > 8:
             return date_.year+1
-        else
+        else:
             return date_.year 
     
 
@@ -201,7 +230,7 @@ class StudyType(object):
     ZS = StudyType('Základná škola', 'ZS', [None] + seq(1,10))
     SS = StudyType('Stredná škola', 'SS', [None] + seq(10,14))
     VS = StudyType('Vysoká škola', 'SS', [None] + seq(14,19))
-    8G = StudyType('8-ročné gymnázium', '8G', [None] + seq(6,14))
+    G8 = StudyType('8-ročné gymnázium', 'G8', [None] + seq(6,14))
     BL = StudyType('Bilingválne gymnázium', 'BL', [None, 10] + seq(10,14))
         
     def __init__(self, name, abbr, grade_to_absolute):
@@ -224,4 +253,5 @@ class StudyTypeField(models.CharField):
         
     def get_prep_value(self, value):
         return value.abbr
+'''
         
