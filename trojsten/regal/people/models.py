16deaf9b9ab331c68fcad4f4ab0ebcb0e56bcf34
Roman Hudec <black3r@klikni.cz>
2013-12-23 17:14:44 +0100

    Pridane relationship-y Person<->User, Task->Round + PEP-8

--- trojsten/regal/people/models.py
+++ trojsten/regal/people/models.py
@@ -7,6 +7,7 @@ from datetime import date
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
 import django.utils.timezone
 
 
@@ -15,8 +16,10 @@ class Address(models.Model):
     street = models.CharField(max_length=64, verbose_name='ulica')
     number = models.CharField(max_length=16, verbose_name='číslo')
     town = models.CharField(max_length=64, db_index=True, verbose_name='mesto')
-    postal_code = models.CharField(max_length=16, db_index=True, verbose_name='PSČ')
-    country = models.CharField(max_length=32, db_index=True, verbose_name='krajina')
+    postal_code = models.CharField(
+        max_length=16, db_index=True, verbose_name='PSČ')
+    country = models.CharField(
+        max_length=32, db_index=True, verbose_name='krajina')
 
     def __str__(self):
         return (
@@ -31,10 +34,13 @@ class Address(models.Model):
 
 @python_2_unicode_compatible
 class Person(models.Model):
+
     '''
     Holds, provide access to or manages all informations
     related to a person.
     '''
+    user = models.OneToOneField(
+        User, primary_key=True, verbose_name='používateľ')
     name = models.CharField(
         max_length=128,  db_index=True, verbose_name='meno')
     surname = models.CharField(
@@ -46,11 +52,11 @@ class Person(models.Model):
                                      null=True,
                                      verbose_name='domáca adresa')
     mailing_address = models.ForeignKey(Address,
-                                               related_name='accepting_mails_here',
-                                               null=True,
-                                               verbose_name='adresa korešpondencie')
+                                        related_name='accepting_mails_here',
+                                        null=True,
+                                        verbose_name='adresa korešpondencie')
     email = models.EmailField(verbose_name='e-mail')
-    
+
     def __str__(self):
         return '%s %s' % (self.name, self.surname)
 
