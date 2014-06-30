5aefa8d53eb7115ec93ce1d6a507d51241658e98
Michal Hozza <mhozza@gmail.com>
2014-06-30 17:28:27 +0200

    _get_submits tests

--- trojsten/results/tests.py
+++ trojsten/results/tests.py
@@ -5,8 +5,12 @@ from trojsten.regal.contests.models import Competition, Series, Round
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 import datetime
-
+from random import randrange
 from trojsten.results import views
+
+
+def sumlen(l):
+    return sum(len(i) for i in l)
 
 
 class ResultsTestCase(TestCase):
@@ -20,7 +24,9 @@ class ResultsTestCase(TestCase):
         self.users = [User.objects.create(username=username) for username in usernames]
         # create series
         self.competition = Competition.objects.create(name='test')
-        self.series = Series.objects.create(competition=self.competition, name='test', number=1, year=1)
+        self.series = Series.objects.create(
+            competition=self.competition, name='test', number=1, year=1
+        )
         # create rounds
         round_names = [1, 2]
         self.rounds = [
@@ -29,9 +35,14 @@ class ResultsTestCase(TestCase):
             ) for i in round_names
         ]
         # create tasks
-        self.submit_types = {name: SubmitType.objects.create(name=name) for name in ['source', 'description']}
+        self.submit_types = {
+            name: SubmitType.objects.create(name=name)
+            for name in ['source', 'description']
+        }
         self.categories = {
-            name: Category.objects.create(name=name, competition=self.competition) for name in ['z', 'o']
+            name: Category.objects.create(
+                name=name, competition=self.competition
+            ) for name in ['z', 'o']
         }
         self.task_cnt = 5
         self.tasks = [
@@ -45,28 +56,40 @@ class ResultsTestCase(TestCase):
             t.category.add(self.categories['z'])
             t.category.add(self.categories['o'])
         # create submits
-        descriptions = [
+        self.descriptions = [
             [1, 2, 3],
             [1, 2, 3],
             [i for i in range(self.task_cnt)],
-            [1, 2, 3, 4, 5],
-            [4, 5, 6],
+            [1, 2, 4, 5],
+            [4, 5],
         ]
-        sources = [
-            [1, 2, 3],
+        self.sources = [
+            [4, 5],
             [1, 2, 3],
             [i for i in range(self.task_cnt)],
-            [1, 2, 3, 4, 5],
-            [4, 5, 6],
+            [2, 3, 5],
+            [1, 2, 3],
         ]
         self.submits = [
             Submit.objects.create(
-                points=10, task=task, user=self.users[i], submit_type=self.submit_types['description']
-            ) for i, tasks in enumerate(descriptions) for task in self.tasks
+                points=i,
+                task=self.tasks[task],
+                user=self.users[i],
+                submit_type=self.submit_types['description'],
+            )
+            for i, tasks in enumerate(self.descriptions)
+            for task in tasks
+            for _ in range(randrange(1,10))
         ] + [
             Submit.objects.create(
-                points=10, task=task, user=self.users[i], submit_type=self.submit_types['source']
-            ) for i, tasks in enumerate(sources) for task in self.tasks
+                points=i,
+                task=self.tasks[task],
+                user=self.users[i],
+                submit_type=self.submit_types['source'],
+            )
+            for i, tasks in enumerate(self.sources)
+            for task in tasks
+            for _ in range(randrange(1,10))
         ]
 
     def test_get_tasks_single_round_no_category(self):
@@ -103,7 +126,9 @@ class ResultsTestCase(TestCase):
 
     def test_get_tasks_single_round_multi_category(self):
         # test count
-        tasks = views._get_tasks(str(self.rounds[0].id), '%d,%d' % (self.categories['z'].id, self.categories['o'].id))
+        tasks = views._get_tasks(
+            str(self.rounds[0].id), '%d,%d' % (self.categories['z'].id, self.categories['o'].id)
+        )
 
         # test all belong to one round
         self.assertEqual(len(tasks), self.task_cnt)
@@ -131,7 +156,9 @@ class ResultsTestCase(TestCase):
 
     def test_get_tasks_multi_round_one_category(self):
         # test count
-        tasks = views._get_tasks('%s,%s' % (self.rounds[0].id, self.rounds[1].id), str(self.categories['z'].id))
+        tasks = views._get_tasks(
+            '%s,%s' % (self.rounds[0].id, self.rounds[1].id), str(self.categories['z'].id)
+        )
 
         # test sorted
         last = None
@@ -158,18 +185,28 @@ class ResultsTestCase(TestCase):
 
     def test_response(self):
         client = Client(HTTP_HOST='ksp.sk')
-        response = client.get(reverse('view_results', kwargs={'round_ids': '%s' % self.rounds[0].id}))
-        self.assertEqual(response.status_code, 200)
-
         response = client.get(
-            reverse('view_results', kwargs={'round_ids': '%s,%s' % (self.rounds[0].id, self.rounds[1].id)})
+            reverse('view_results', kwargs={'round_ids': '%s' % self.rounds[0].id})
         )
         self.assertEqual(response.status_code, 200)
 
         response = client.get(
             reverse(
                 'view_results',
-                kwargs={'round_ids': '%s' % self.rounds[0].id, 'category_ids': '%s' % self.categories['z'].id},
+                kwargs={
+                    'round_ids': '%s,%s' % (self.rounds[0].id, self.rounds[1].id)
+                },
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response = client.get(
+            reverse(
+                'view_results',
+                kwargs={
+                    'round_ids': '%s' % self.rounds[0].id,
+                    'category_ids': '%s' % self.categories['z'].id
+                },
             )
         )
         self.assertEqual(response.status_code, 200)
@@ -208,7 +245,9 @@ class ResultsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_submits_single_task(self):
-        task = Task.objects.create(number=47, round=self.rounds[0], name='', description_points=10, source_points=10)
+        task = Task.objects.create(
+            number=47, round=self.rounds[0], name='', description_points=10, source_points=10
+        )
         task.task_types.add(self.submit_types['source'])
         submit_cnt = 10
         submits = [
@@ -220,17 +259,50 @@ class ResultsTestCase(TestCase):
         self.assertEqual(len(task_submits), 1)
         self.assertEqual(task_submits[0], submits[-1])
 
-        task = Task.objects.create(number=42, round=self.rounds[0], name='', description_points=10, source_points=10)
+        task = Task.objects.create(
+            number=42, round=self.rounds[0], name='', description_points=10, source_points=10
+        )
         task.task_types.add(self.submit_types['description'])
         submit_cnt = 10
         submits = [
             Submit.objects.create(
-                points=10, task=task, user=self.users[0], submit_type=self.submit_types['source']
+                points=10,
+                task=task,
+                user=self.users[0],
+                submit_type=self.submit_types['description'],
             ) for _ in range(submit_cnt)
         ]
         task_submits = views._get_submits([task])
         self.assertEqual(len(task_submits), 1)
         self.assertEqual(task_submits[0], submits[-1])
+
+        task = Task.objects.create(
+            number=49, round=self.rounds[0], name='', description_points=10, source_points=10
+        )
+        task.task_types.add(self.submit_types['source'])
+        task.task_types.add(self.submit_types['description'])
+        submit_cnt = 10
+        submits_s = [
+            Submit.objects.create(
+                points=10, task=task, user=self.users[0], submit_type=self.submit_types['source']
+            ) for _ in range(submit_cnt)
+        ]
+        submits_d = [
+            Submit.objects.create(
+                points=10,
+                task=task,
+                user=self.users[0],
+                submit_type=self.submit_types['description'],
+            ) for _ in range(submit_cnt)
+        ]
+        task_submits = views._get_submits([task])
+        self.assertEqual(len(task_submits), 2)
+        self.assertTrue(submits_d[-1] in task_submits)
+        self.assertTrue(submits_s[-1] in task_submits)
+
+    def test_get_submits_multi_task(self):
+        task_submits = views._get_submits(self.tasks)
+        self.assertEqual(len(task_submits), sumlen(self.sources) + sumlen(self.descriptions))
 
     def test_results_single_round(self):
         pass
