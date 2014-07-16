e0f112d9a4c211ad00e1bc20866de8c26fba2df3
Michal Hozza <mhozza@gmail.com>
2014-07-16 22:24:28 +0200

    admin and requirements

--- trojsten/regal/contests/admin.py
+++ trojsten/regal/contests/admin.py
@@ -6,6 +6,10 @@ from django.contrib import admin
 
 from trojsten.regal.contests.models import *
 
+class RepositoryAdmin(admin.ModelAdmin):
+    readonly_fields=('notification_string',)
+
 admin.site.register(Competition)
 admin.site.register(Series)
 admin.site.register(Round)
+admin.site.register(Repository, RepositoryAdmin)
