ea37da986061e45f1f57f9d8500daa8da5be9966
Michal Hozza <mhozza@gmail.com>
2014-07-16 22:40:59 +0200

    slug in admin

--- trojsten/news/admin.py
+++ trojsten/news/admin.py
@@ -5,6 +5,7 @@ from trojsten.news.models import Entry, Label
 
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date', 'author')
+    readonly_fields = ('slug',)
     exclude = ('author',)
 
     def save_model(self, request, obj, form, change):
