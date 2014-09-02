4fa5c5776a8cf7fbced8d75632ecc6b4fcab48fb
Michal Hozza <mhozza@gmail.com>
2014-09-02 14:20:46 +0200

    Highlighting current page in menu

--- trojsten/utils/templatetags/utils.py
+++ trojsten/utils/templatetags/utils.py
@@ -1,4 +1,6 @@
 from django import template
+from django.core import urlresolvers
+import re
 
 register = template.Library()
 
@@ -11,3 +13,27 @@ def lookup(d, key):
 @register.filter
 def split(value, arg):
     return value.split(arg)
+
+
+@register.simple_tag(takes_context=True)
+def current(context, url_name, return_value='active', **kwargs):
+    matches = current_url_equals(context, url_name, **kwargs)
+    return return_value if matches else ''
+
+
+def current_url_equals(context, url_name, **kwargs):
+    resolved = False
+    try:
+        resolved = urlresolvers.resolve(context.get('request').path)
+    except:
+        pass
+    matches = resolved and resolved.url_name == url_name
+    if matches and kwargs:
+        for key in kwargs:
+            kwarg = kwargs.get(key)
+            resolved_kwarg = resolved.kwargs.get(key)
+            if not resolved_kwarg or kwarg != resolved_kwarg:
+                return False
+    if not matches:
+        return re.search(url_name, context.get('request').path)
+    return matches
