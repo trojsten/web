2164f25bdd9eb947d494ca281830cdfe4e18bce4
Michal Hozza <mhozza@gmail.com>
2014-09-05 18:11:53 +0200

    Using Django UserAdmin with our custom fields

--- trojsten/regal/people/admin.py
+++ trojsten/regal/people/admin.py
@@ -3,9 +3,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from trojsten.regal.people.models import *
+
+
+class UserAdmin(DefaultUserAdmin):
+    def __init__(self, *args, **kwargs):
+        super(UserAdmin, self).__init__(*args, **kwargs)
+        self.fieldsets += (('Extra', {'fields': ('gender', 'birth_date', 'home_address', 'mailing_address', 'school', 'graduation')}),)
 
 admin.site.register(Address)
 admin.site.register(School)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
