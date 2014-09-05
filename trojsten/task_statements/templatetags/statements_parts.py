e21eaf4f1e00ece0e9b8fe84ba6fe32ca0a39831
Michal Hozza <mhozza@gmail.com>
2014-09-05 13:20:57 +0200

    Fixed task ordering

--- trojsten/task_statements/templatetags/statements_parts.py
+++ trojsten/task_statements/templatetags/statements_parts.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.inclusion_tag('trojsten/task_statements/parts/task_list.html')
 def show_task_list(round_id):
-    tasks = Task.objects.filter(round_id=round_id)
+    tasks = Task.objects.filter(round_id=round_id).order_by('number')
     data = {
         'tasks': tasks,
     }
