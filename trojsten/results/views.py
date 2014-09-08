4691d0074e9a08eb48bdd4431b91318673cccd50
Michal Hozza <mhozza@gmail.com>
2014-09-08 13:59:24 +0200

    Unified page headers support

--- trojsten/results/views.py
+++ trojsten/results/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .helpers import get_tasks, get_submits, get_results_data, make_result_table
-from trojsten.regal.tasks.models import Submit
+from trojsten.regal.tasks.models import Submit, Category
+from trojsten.regal.contests.models import Round
 
 
 def view_results(request, round_ids, category_ids=None):
     '''Displays results for specified round_ids and category_ids
     '''
+    rounds = Round.objects.filter(
+        pk__in=round_ids.split(',')
+    )
+    categories = None if category_ids is None else Category.objects.filter(
+        pk__in=category_ids.split(',')
+    )
     tasks = get_tasks(round_ids, category_ids)
     submits = get_submits(tasks)
     results_data = get_results_data(tasks, submits)
@@ -13,6 +20,8 @@ def view_results(request, round_ids, category_ids=None):
 
     template_data = {
         'Submit': Submit,
+        'rounds': rounds,
+        'categories': categories,
         'tasks': tasks,
         'results': results,
     }
