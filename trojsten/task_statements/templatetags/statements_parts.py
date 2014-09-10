0e55ea8a40b872303b0020ebee56da183f31540f
Michal Hozza <mhozza@gmail.com>
2014-09-10 23:43:44 +0200

    Simple results link in task statements

--- trojsten/task_statements/templatetags/statements_parts.py
+++ trojsten/task_statements/templatetags/statements_parts.py
@@ -1,5 +1,6 @@
 from django import template
 from trojsten.regal.tasks.models import Task
+from ..helpers import get_result_rounds
 
 register = template.Library()
 
@@ -9,5 +10,15 @@ def show_task_list(round_id):
     tasks = Task.objects.filter(round_id=round_id).order_by('number')
     data = {
         'tasks': tasks,
+    }
+    return data
+
+
+@register.inclusion_tag('trojsten/task_statements/parts/buttons.html')
+def show_buttons(round):
+    result_rounds = get_result_rounds(round)
+    data = {
+        'round': round,
+        'result_rounds': result_rounds,
     }
     return data
