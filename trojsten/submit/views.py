78dc8fb20a84ae27dfb0add04d3424eb2ac28b9e
Roman Hudec <black3r@klikni.cz>
2014-09-22 15:39:39 +0200

    correctly handle incorrect file format

--- trojsten/submit/views.py
+++ trojsten/submit/views.py
@@ -147,21 +147,24 @@ def task_submit_post(request, task_id, submit_type):
                 language = '.zip'
             # Source submit's should be processed by process_submit()
             submit_id = process_submit(sfile, task, language, request.user)
-            # Source file-name is id.data
-            sfiletarget = os.path.join(get_path(
-                task, request.user), submit_id + '.data')
-            save_file(sfile, sfiletarget)
-            sub = Submit(task=task,
-                         user=request.user,
-                         submit_type=submit_type,
-                         points=0,
-                         filepath=sfiletarget,
-                         testing_status='in queue',
-                         protocol_id=submit_id)
-            sub.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "Úspešne si submitol program, výsledok testovania nájdeš <a href='%s'>tu</a>" %
-                                 reverse("view_submit", args=[sub.id]))
+            if not submit_id:
+                messages.add_message(request, messages.WARNING, "Nepodporovaný formát súboru")
+            else:
+                # Source file-name is id.data
+                sfiletarget = os.path.join(get_path(
+                    task, request.user), submit_id + '.data')
+                save_file(sfile, sfiletarget)
+                sub = Submit(task=task,
+                             user=request.user,
+                             submit_type=submit_type,
+                             points=0,
+                             filepath=sfiletarget,
+                             testing_status='in queue',
+                             protocol_id=submit_id)
+                sub.save()
+                messages.add_message(request, messages.SUCCESS,
+                                     "Úspešne si submitol program, výsledok testovania nájdeš <a href='%s'>tu</a>" %
+                                     reverse("view_submit", args=[sub.id]))
         else:
             for field in form:
                 for error in field.errors:
