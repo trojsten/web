ffb276b7593b41b2362725732500fab2d5790600
Michal Petrucha <michal.petrucha@ksp.sk>
2014-09-23 22:30:45 +0200

    Untested partial implementation of people migration.

--- trojsten/regal/people/management/commands/migrate_people_from_kaspar.py
+++ trojsten/regal/people/management/commands/migrate_people_from_kaspar.py
@@ -9,6 +9,14 @@ from trojsten.regal.people.models import (User, UserProperty, Address,
     School)
 
 
+# Kaspar property IDs
+EMAIL_PROP = 1
+BIRTHDAY_PROP = 2
+# Labels for auto-generated properties
+KASPAR_ID_LABEL = "kaspar ID"
+KASPAR_NOTE_LABEL = "kaspar note"
+
+
 class Command(NoArgsCommand):
     help = 'Imports people and their related info from kaspar.'
 
@@ -51,6 +59,53 @@ class Command(NoArgsCommand):
                     school_id_map[kaspar_id] = choices[choice]
             else:
                 school_id_map[kaspar_id] = self.create_school(*row)
+
+        if self.verbosity >= 1:
+            self.stdout.write("Migrating people...")
+
+        c.execute("""
+            SELECT man_id, firstname, lastname, school_id, finish, note
+            FROM people;
+        """)
+        man_id_map = dict()
+        for row in c:
+            man_id, first_name, last_name, school_id = row[0:4]
+            grad_year, note = row[4:]
+
+            new_user_args = {
+                'first_name': first_name,
+                'last_name': last_name,
+                'username': first_name + last_name,
+                'is_active': False,
+            }
+
+            if grad_year:
+                new_user_args['graduation'] = grad_year
+
+            # The following takes O(N) queries and I don't care --
+            # it's a one-time background job anyway.
+            d = kaspar.cursor()
+            d.execute("""
+                SELECT ppt_id, value
+                FROM people_prop
+                WHERE man_id = ? AND ppt_id IN [?, ?]
+            """, (man_id, EMAIL_PROP, BIRTHDAY_PROP)
+            for prop_id, value in d:
+                if prop_id == EMAIL_PROP:
+                    new_user_args['email'] = value
+                elif prop_id == BIRTHDAY_PROP:
+                    new_user_args['birth_date'] = value
+            d.close()
+            del d
+
+            new_user = User.objects.create(**new_user_args)
+            man_id_map[man_id] = new_user
+
+            new_user.properties.create(key=KASPAR_ID_LABEL,
+                                       value=man_id)
+            if note:
+                new_user.properties.create(key=KASPAR_NOTE_LABEL,
+                                           value=note)
 
     def create_school(self, kaspar_id, abbr, name, addr_name, street,
                       city, zip_code):
