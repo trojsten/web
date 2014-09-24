dd710ffc65a6aeca8b6e7665cddfab7c9c50b6c5
Michal Hozza <mhozza@gmail.com>
2014-09-24 13:17:43 +0200

    redirect to news on / when signed in

--- trojsten/views.py
+++ trojsten/views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from wiki.views import article
 
 
 def contact_form_sent_redirect(request):
     messages.success(request, 'Vaša správa bola odoslaná. Ďakujeme za spätnú väzbu.')
     return redirect('/')
+
+
+def home_redirect(request):
+    if request.user.is_authenticated():
+        if 'home' not in request.GET:
+            return redirect(reverse('news_list', kwargs={'page': 1}))
+    return article.ArticleView.as_view()(request, path='')
