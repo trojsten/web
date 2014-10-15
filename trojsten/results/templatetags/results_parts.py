0e85bfeb978f064b24534f5403c01ba6b430e671
Michal Hozza <mhozza@gmail.com>
2014-10-15 12:19:59 +0200

    automatic loading frozen results page

--- trojsten/results/templatetags/results_parts.py
+++ trojsten/results/templatetags/results_parts.py
@@ -1,6 +1,8 @@
 from django import template
-from ..helpers import make_result_table
+from ..helpers import make_result_table, get_frozen_results_path
 from trojsten.regal.tasks.models import Submit
+import json
+import os
 
 register = template.Library()
 
@@ -9,7 +11,14 @@ register = template.Library()
 def show_results_table(context, rounds, categories=None):
     '''Displays results for specified tasks and categories
     '''
-    current_tasks, results = make_result_table(rounds, categories, context['show_staff'])
+    path = get_frozen_results_path(rounds, categories)
+    force_generate = 'force_generate' in context and context['force_generate']
+    if os.path.exists(path) and not force_generate:
+        with open(path) as f:
+            data = json.load(f)
+            current_tasks, results = data['current_tasks'], data['results']
+    else:
+        current_tasks, results = make_result_table(rounds, categories, context['show_staff'])
 
     context.update({
         'Submit': Submit,
