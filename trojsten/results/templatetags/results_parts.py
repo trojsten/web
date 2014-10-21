db847e7fbdb05189f4ded717b5e05caad79d6ee6
Michal Hozza <mhozza@gmail.com>
2014-10-21 22:39:05 +0200

    removed dead freezing code, will be done in next PR

--- trojsten/results/templatetags/results_parts.py
+++ trojsten/results/templatetags/results_parts.py
@@ -1,8 +1,6 @@
 from django import template
-from ..helpers import make_result_table, get_frozen_results_path
+from ..helpers import make_result_table
 from trojsten.regal.tasks.models import Submit
-import json
-import os
 
 register = template.Library()
 
@@ -11,20 +9,7 @@ register = template.Library()
 def show_results_table(context, rounds, categories=None):
     '''Displays results for specified tasks and categories
     '''
-    path = get_frozen_results_path(rounds, categories)
-    force_generate = (
-        ('force_generate' in context) and context['force_generate'] and
-        (  # only organizer can force generate results
-            context['user'].is_superuser or
-            rounds[0].series.competition.organizers_group in context['user'].groups.all()
-        )
-    )
-    if os.path.exists(path) and not force_generate:
-        with open(path) as f:
-            data = json.load(f)
-            current_tasks, results = data['current_tasks'], data['results']
-    else:
-        current_tasks, results = make_result_table(rounds, categories, context['show_staff'])
+    current_tasks, results = make_result_table(rounds, categories, context['show_staff'])
 
     context.update({
         'Submit': Submit,
@@ -33,16 +18,3 @@ def show_results_table(context, rounds, categories=None):
         'has_previous_results': len(rounds) > 1
     })
     return context
-
-
-@register.assignment_tag
-def is_final(rounds, categories=None):
-    return os.path.exists(get_frozen_results_path(rounds, categories))
-
-
-@register.assignment_tag(takes_context=True)
-def is_organizer(context, competition):
-    return (
-        context['user'].is_superuser or
-        competition.organizers_group in context['user'].groups.all()
-    )
--- trojsten/results/views.py
+++ trojsten/results/views.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
-from django.contrib import messages
-from .helpers import (check_round_series, make_result_table,
-                      get_frozen_results_path, ResultsEncoder, has_permission)
+from django.shortcuts import render
+from .helpers import check_round_series
 from trojsten.regal.tasks.models import Category
 from trojsten.regal.contests.models import Round
 from django.http import HttpResponseBadRequest
-from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
-import json
 
 
 def view_results(request, round_ids, category_ids=None):
@@ -64,42 +58,3 @@ def view_latest_results(request):
     return render(
         request, 'trojsten/results/view_latest_results.html', template_data
     )
-
-
-@login_required
-def freeze_results(request, round_ids, category_ids=None):
-
-    rounds = Round.objects.filter(
-        pk__in=round_ids.split(',')
-    ).select_related('series__competition__organizers_group')
-    if rounds or not check_round_series(rounds):
-        return HttpResponseBadRequest()
-
-    if not has_permission(
-        rounds[0].series.competition.organizers_group, request.user
-    ):
-        raise PermissionDenied
-
-    categories = None if not category_ids else Category.objects.filter(
-        pk__in=category_ids.split(',')
-    )
-
-    current_tasks, results = make_result_table(rounds, categories)
-    path = get_frozen_results_path(rounds, categories)
-
-    #serialize data
-    with open(path, 'w') as f:
-        json.dump({
-            'current_tasks': list(current_tasks),
-            'results': results,
-        }, f, cls=ResultsEncoder, sort_keys=True, indent=4, separators=(',', ': '),)
-
-    messages.add_message(
-        request,
-        messages.SUCCESS,
-        "Výsledkovka bola úspešne zmrazená do súboru: %s" % path,
-    )
-    return redirect(reverse(
-        'view_results',
-        kwargs={'round_ids': round_ids, 'category_ids': category_ids},
-    ))
