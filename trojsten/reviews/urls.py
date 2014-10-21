15d23a7e39071a00ace7fc02e2a817dba710e486
Lukáš Ivan <kuko0411@gmail.com>
2014-10-21 20:46:11 +0200

    Added option to upload single review

--- trojsten/reviews/urls.py
+++ trojsten/reviews/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from trojsten.reviews.views import task_view, task_user_view, submit_download_view, download_latest_submits_view
+from trojsten.reviews.views import review_task_view, task_user_view, submit_download_view, download_latest_submits_view
 from django.contrib import admin
 
 task_review_urls = [
     url(r'^(?P<task_pk>[0-9]+)/download_latest_submits$', admin.site.admin_view(download_latest_submits_view), name="download_latest_submits"),
-    url(r'^(?P<task_pk>[0-9]+)/review/$', admin.site.admin_view(task_view), name="review_task"),
+    url(r'^(?P<task_pk>[0-9]+)/review/$', admin.site.admin_view(review_task_view), name="review_task"),
     url(r'^(?P<task_pk>[0-9]+)/review/(?P<user_pk>(all|[0-9]+))$', admin.site.admin_view(task_user_view), name="review_task_user"),
 ]
 
--- trojsten/reviews/views.py
+++ trojsten/reviews/views.py
@@ -1,17 +1,78 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.core.urlresolvers import reverse
+# -*- coding: utf-8 -*-
+from django.contrib import messages
 
-from trojsten.regal.tasks.models import Task, Submit
+from trojsten.regal.tasks.models  import Task, Submit
+from trojsten.regal.people.models import User
+from trojsten.submit.helpers import save_file, get_path
+
 from django import forms
+import os
+# the code will be later refractored to proper files
 
-def task_view (request, task_pk):
+def submit_review (filecontent, filename, task, user):
+    from time import time
+    submit_id = str(int(time()))
+    
+    sfiletarget = os.path.join(
+        get_path(task, user),
+        "%s-%s-%s" % (user.last_name, submit_id, filename),
+    )
+
+    save_file(filecontent, sfiletarget)
+    sub = Submit(task=task,
+                 user=user,
+                 submit_type=Submit.REVIEW,
+                 points=0,
+                 testing_status='in queue',
+                 filepath=sfiletarget)
+    sub.save()
+
+
+
+
+def review_task_view (request, task_pk):
     task = get_object_or_404(Task, pk=task_pk)
     users = get_users(task)
+    choices = [("auto", "Auto"), ("all", "All")] + [(str(user.pk), user.username) for user in users]
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES, users=choices)
+
+        if form.is_valid():
+            user = None
+
+            if form.cleaned_data["user"] == "all":
+                #not implemented
+                raise Http404
+            else:
+                if form.cleaned_data["user"] == "auto":
+                    filename = form.cleaned_data["file"].name
+                    try:
+                        form.cleaned_data["user"] = Submit.objects.get(pk=int(filename.split('-')[1])).user.pk
+                    except KeyError:
+                        messages.add_message(request, messages.ERROR, "Auto failed")                        
+                
+                user = User.objects.get(pk=int(form.cleaned_data["user"]))
+                #TODO: add regex to check for Name-submitID-filename format
+
+                fname = "".join(form.cleaned_data["file"].name.split("-")[2:])
+                fcont = request.FILES['file']
+
+                submit_review(fcont, fname, task, user)
+
+                messages.add_message(request, messages.SUCCESS,
+                                 "Uploadnute")
+
+                return redirect("admin:review_task", task.pk)
+
 
     template_data = {
         'task': task,
         'users': users,
+        'form' : ReviewForm(users=choices),
     }
 
     return render (
@@ -59,12 +120,12 @@ def get_users (task):
 
 
 def submit_readable_name (submit):
-	return '%s_%s_%s' % (str(submit.user.username), str(submit.pk), submit.filename.split('-')[-1])
+    return '%s-%s-%s' % (str(submit.user.last_name), str(submit.pk), "".join(submit.filename.split('-')[2:]))
 
 
 def submit_download_view (request, submit_pk):
     submit = get_object_or_404(Submit, pk=submit_pk)
-    name = submit(submit_readable_name(submit))
+    name = submit_readable_name(submit)
 
     response = HttpResponse(open(submit.filepath).read(), content_type='plain/text')
     response['Content-Disposition'] = 'attachment; filename=' + name
@@ -90,3 +151,16 @@ def download_latest_submits_view (request, task_pk):
     response = HttpResponse(result.read(),content_type='plain/text')
     response['Content-Disposition'] = 'attachment; filename=Uloha %s.zip' % task.name 
     return response
+
+
+from django import forms
+
+class ReviewForm (forms.Form):
+    file = forms.FileField(max_length=128)
+    user = forms.ChoiceField ()
+
+    def __init__ (self, *args, **kwargs):
+        users = kwargs.pop("users")
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        self.fields["user"].choices = users
