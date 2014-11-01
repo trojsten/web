b5a3435711d80e593ac9887892e890243ca1c68a
Michal Hozza <mhozza@gmail.com>
2014-11-01 14:13:47 +0100

    used detail view instead of templateview

--- trojsten/regal/events/views.py
+++ trojsten/regal/events/views.py
@@ -1,25 +1,12 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 
 from .models import Event
 
 
-class ParticipantsAndOrganizersListView(TemplateView):
+class ParticipantsAndOrganizersListView(DetailView):
     template_name = "trojsten/regal/events/participants_and_organizers_list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(
-            ParticipantsAndOrganizersListView, self
-        ).get_context_data(**kwargs)
-
-        event = Event.objects.get(pk=kwargs['event_id'])
-        participants = event.participants.all()
-        organizers = event.organizers.all()
-
-        context.update({
-            'event': event,
-            'participants': participants,
-            'organizers': organizers,
-        })
-        return context
+    model = Event
+    context_object_name = 'event'
+    pk_url_kwarg = 'event_id'
 
 participants_organizers_list = ParticipantsAndOrganizersListView.as_view()
