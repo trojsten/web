601fcfbe5132d3c55325b309e1202c098aa052dc
Michal Hozza <mhozza@gmail.com>
2014-11-01 18:19:22 +0100

    fixed field dict keys for dynamic fields

--- trojsten/regal/events/forms.py
+++ trojsten/regal/events/forms.py
@@ -6,6 +6,8 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
+    PROP_FIELD_NAME = 'prop_%d'
+
     going = forms.TypedChoiceField(
         label='Prihlasujem sa ako %s',
         coerce=lambda x: x == 'True',
@@ -18,4 +20,4 @@ class RegistrationForm(forms.Form):
         self.invite = invite
         self.fields['going'].label %= self.invite.get_type_display()
         for prop in self.invite.event.registration.required_user_properties.all():
-            self.fields[prop] = forms.CharField(label=prop.key_name)
+            self.fields[RegistrationForm.PROP_FIELD_NAME % prop.id] = forms.CharField(label=prop.key_name, required=False)
