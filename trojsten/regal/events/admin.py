a6d530ea433c86f31090810bdd3ca0e65b5ac9aa
Michal Hozza <mhozza@gmail.com>
2014-11-02 15:03:06 +0100

    Added invitation inlines

--- trojsten/regal/events/admin.py
+++ trojsten/regal/events/admin.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django import forms
 from django.utils.encoding import force_text
 
 from .models import *
@@ -20,8 +21,54 @@ class LinkAdmin(admin.ModelAdmin):
     list_display = ('title', 'url')
 
 
+class ParticipantModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ParticipantModelForm, self).__init__(*args, **kwargs)
+        self.fields['type'].choices = [
+            choice for choice in self.fields['type'].choices
+            if choice[0] != Invitation.ORGANIZER
+        ]
+
+
+class ParticipantInvitationInline(admin.StackedInline):
+    model = Invitation
+    form = ParticipantModelForm
+    extra = 1
+    fields = (('user', 'type'),)
+    verbose_name = 'účastník'
+    verbose_name_plural = 'účastníci'
+
+    def get_queryset(self, request):
+        qs = super(ParticipantInvitationInline, self).get_queryset(request)
+        return qs.exclude(type=Invitation.ORGANIZER)
+
+
+class OrganizerModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(OrganizerModelForm, self).__init__(*args, **kwargs)
+        self.fields['type'].initial = Invitation.ORGANIZER
+        self.fields['type'].widget = forms.widgets.HiddenInput()
+        self.fields['type'].label = ''
+        self.fields['user'].label = 'Vedúci:'
+
+
+class OrganizerInvitationInline(admin.StackedInline):
+    form = OrganizerModelForm
+    model = Invitation
+    extra = 1
+    fields = (('user', 'type'),)
+    verbose_name = 'vedúci'
+
+    def get_queryset(self, request):
+        qs = super(OrganizerInvitationInline, self).get_queryset(request)
+        return qs.filter(type=Invitation.ORGANIZER)
+
+
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'place', 'start_time', 'end_time')
+    inlines = [
+        ParticipantInvitationInline, OrganizerInvitationInline
+    ]
 
 
 class InvitationAdmin(admin.ModelAdmin):
