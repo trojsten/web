2c5fff3612532a5403357827b52c6f3c30698be4
Michal Hozza <mhozza@gmail.com>
2014-11-05 23:13:46 +0100

    after-rebase fixes

--- trojsten/urls.py
+++ trojsten/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 
 from wiki.urls import get_pattern as get_wiki_pattern
-from django_notify.urls import get_pattern as get_notify_pattern
+from django_nyt.urls import get_pattern as get_notify_pattern
 from contact_form.views import ContactFormView
 
 import trojsten.submit.urls
@@ -13,14 +13,8 @@ import trojsten.news.urls
 import trojsten.task_statements.urls
 from trojsten.regal.people.forms import TrojstenUserCreationForm, TrojstenUserChangeForm
 
-
 admin.autodiscover()
 admin.site.login = login_required(admin.site.login)
-
-from wiki.urls import get_pattern as get_wiki_pattern
-from django_nyt.urls import get_pattern as get_notify_pattern
-from trojsten.regal.people.forms import TrojstenUserCreationForm, TrojstenUserChangeForm
-from contact_form.views import ContactFormView
 
 # Override default forms in ksp_login
 urlpatterns = patterns('ksp_login.views',
