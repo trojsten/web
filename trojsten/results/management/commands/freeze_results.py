c0f2f931f621f1f588f9f5be4f26374c0e0f9751
Michal Hozza <mhozza@gmail.com>
2014-12-02 00:19:16 +0100

    Working results freezing command

--- trojsten/results/management/commands/freeze_results.py
+++ trojsten/results/management/commands/freeze_results.py
@@ -1,12 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from django.contrib.auth.models import AnonymousUser
 
 from trojsten.regal.contests.models import Round
-from trojsten.results.models import FrozenResults
+from trojsten.results.models import FrozenResults, FrozenUserResult
+from trojsten.results.helpers import make_result_table
 
 
 class Command(BaseCommand):
     args = '<round_id round_id ...>'
 
+    @transaction.atomic
     def handle(self, *args, **options):
         for round_id in args:
             try:
@@ -21,6 +25,32 @@ class Command(BaseCommand):
                         is_single_round=single_round,
                         category=category
                     )
-                    frozen_results.save()
+
+                    results_table = make_result_table(AnonymousUser(), round, category, single_round)
+
+                    for result in results_table.results_data:
+                        frozen_result = FrozenUserResult.objects.create(
+                            frozenresults=frozen_results,
+                            original_user=result.user,
+                            rank=result.data.rank,
+                            fullname=result.user.get_full_name(),
+                            school_year=result.user.school_year,
+                            school=result.user.school,
+                            previous_points=result.data.previous_rounds_points,
+                            sum=result.data.sum,
+                        )
+
+                        tasks = filter(
+                            lambda task: result.data.tasks[task.id].submitted,
+                            results_table.tasks,
+                        )
+
+                        for task in tasks:
+                            frozen_result.task_points.create(
+                                task=task,
+                                description_points=result.data.tasks[task.id].description_points,
+                                source_points=result.data.tasks[task.id].source_points,
+                                sum=result.data.tasks[task.id].sum,
+                            )
 
             self.stdout.write('Round "%s" successfully frozen' % round_id)
