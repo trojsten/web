86d3669b993f1ba213b59dd3fe4bcc140aa05e72
Michal Hozza <mhozza@gmail.com>
2014-12-01 23:39:26 +0100

    allowed no category in frozen results

--- trojsten/results/models.py
+++ trojsten/results/models.py
@@ -11,7 +11,7 @@ from django.conf import settings
 class FrozenResults(models.Model):
     round = models.ForeignKey('contests.Round', verbose_name='kolo')
     is_single_round = models.BooleanField(verbose_name='vynechať predošlé kolá')
-    category = models.ForeignKey('tasks.Category', verbose_name='kolo')
+    category = models.ForeignKey('tasks.Category', blank=True, null=True, verbose_name='kolo')
 
     class Meta:
         verbose_name = 'Zmrazená výsledkovka'
