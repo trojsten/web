e84309bfd995ca218788ef3f8865cae15f146145
Michal Hozza <mhozza@gmail.com>
2014-12-02 11:12:43 +0100

    split to multiple methods

--- trojsten/results/management/commands/freeze_results.py
+++ trojsten/results/management/commands/freeze_results.py
@@ -10,7 +10,44 @@ from trojsten.results.helpers import make_result_table
 class Command(BaseCommand):
     args = '<round_id round_id ...>'
 
+    def freeze_result(self, frozen_results, tasks, result):
+        frozen_result = FrozenUserResult.objects.create(
+            frozenresults=frozen_results,
+            original_user=result.user,
+            rank=result.data.rank,
+            fullname=result.user.get_full_name(),
+            school_year=result.user.school_year,
+            school=result.user.school,
+            previous_points=result.data.previous_rounds_points,
+            sum=result.data.sum,
+        )
+
+        for task in tasks:
+            frozen_result.task_points.create(
+                task=task,
+                description_points=result.data.tasks[task.id].description_points,
+                source_points=result.data.tasks[task.id].source_points,
+                sum=result.data.tasks[task.id].sum,
+            )
+
     @transaction.atomic
+    def freeze_round(self, round, category, single_round):
+        frozen_results = FrozenResults.objects.create(
+            round=round,
+            is_single_round=single_round,
+            category=category
+        )
+
+        results_table = make_result_table(AnonymousUser(), round, category, single_round)
+
+        for result in results_table.results_data:
+            tasks = filter(
+                lambda task: result.data.tasks[task.id].submitted,
+                results_table.tasks,
+            )
+
+            self.freeze_result(frozen_results, tasks, result)
+
     def handle(self, *args, **options):
         for round_id in args:
             try:
@@ -20,37 +57,6 @@ class Command(BaseCommand):
 
             for single_round in (False, True):
                 for category in [None] + list(round.categories):
-                    frozen_results = FrozenResults.objects.create(
-                        round=round,
-                        is_single_round=single_round,
-                        category=category
-                    )
-
-                    results_table = make_result_table(AnonymousUser(), round, category, single_round)
-
-                    for result in results_table.results_data:
-                        frozen_result = FrozenUserResult.objects.create(
-                            frozenresults=frozen_results,
-                            original_user=result.user,
-                            rank=result.data.rank,
-                            fullname=result.user.get_full_name(),
-                            school_year=result.user.school_year,
-                            school=result.user.school,
-                            previous_points=result.data.previous_rounds_points,
-                            sum=result.data.sum,
-                        )
-
-                        tasks = filter(
-                            lambda task: result.data.tasks[task.id].submitted,
-                            results_table.tasks,
-                        )
-
-                        for task in tasks:
-                            frozen_result.task_points.create(
-                                task=task,
-                                description_points=result.data.tasks[task.id].description_points,
-                                source_points=result.data.tasks[task.id].source_points,
-                                sum=result.data.tasks[task.id].sum,
-                            )
+                    self.freeze_round(round, category, single_round)
 
             self.stdout.write('Round "%s" successfully frozen' % round_id)
