a13b9cd33c1a48c37bbb8c01b9a7d200fdaa004c
Roman Hudec <black3r@klikni.cz>
2014-12-28 18:21:52 +0100

    replaced datepicker with bootstrap compatible plug-in

--- trojsten/static/js/settings-datepicker.js
+++ trojsten/static/js/settings-datepicker.js
@@ -3,11 +3,9 @@
     $(function()
     {
         var elem = "input#id_birth_date";
-        $(elem).datepicker({
-            dateFormat: "dd.mm.yy",
-            changeMonth: true,
-            changeYear: true,
-            yearRange: "1930:-6y"
+        $(elem).datetimepicker({
+            language: 'sk',
+            pickTime: false,
         });
     });
 
