dac05a1fe14c3eb0f96e76074951b2e5048c4901
Michal Hozza <mhozza@gmail.com>
2015-01-17 11:18:55 +0100

    lowercase & removed SITE_ID from context processor

--- trojsten/context_processors.py
+++ trojsten/context_processors.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 def current_site(request):
     return {
-        'SITE_ID': settings.SITE_ID,
         'SITE': settings.SITES[settings.SITE_ID],
         'OTHER_SITES': [v for k, v in sorted(settings.SITES.items()) if k != settings.SITE_ID],
     }
