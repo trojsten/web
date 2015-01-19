d03df7e238a920fd88da64630ae70079c82987ab
Roman Hudec <black3r@klikni.cz>
2015-01-19 16:58:14 +0100

    revision + glyphicons link in footer

--- trojsten/context_processors.py
+++ trojsten/context_processors.py
@@ -1,8 +1,13 @@
 from django.conf import settings
-
+from trojsten import version
 
 def current_site(request):
     return {
         'SITE': settings.SITES[settings.SITE_ID],
         'OTHER_SITES': [v for k, v in sorted(settings.SITES.items()) if k != settings.SITE_ID],
+    }
+
+def version_string(request):
+    return {
+        'VERSION': version.version_string,
     }
