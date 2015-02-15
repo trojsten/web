297be45d3dd9fe7c6f89c6ecf7c3dcf82c343cf5
Michal Hozza <mhozza@gmail.com>
2015-02-15 22:55:41 +0100

    debug logging

--- trojsten/settings/development.py
+++ trojsten/settings/development.py
@@ -10,8 +10,10 @@ SENDFILE_BACKEND = 'sendfile.backends.development'
 INSTALLED_APPS += (
     'debug_toolbar',
 )
-MIDDLEWARE_CLASSES = (('debug_toolbar.middleware.DebugToolbarMiddleware',)
-   + MIDDLEWARE_CLASSES)
+MIDDLEWARE_CLASSES = (
+    ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    + MIDDLEWARE_CLASSES
+)
 
 INTERNAL_IPS = ('127.0.0.1',)
 
@@ -21,3 +23,48 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Use localhost for first site route
 SITES[SITE_ID]['URL'] = 'http://localhost:8000'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
