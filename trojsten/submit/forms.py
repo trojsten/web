2032d1a25b64b35b4307e5aedd90bbc73c6f9ed0
Zoltan 'Zoli' Onody <zoli@ksp.sk>
2015-09-13 16:13:41 +0200

    Add translation to submit

--- trojsten/submit/forms.py
+++ trojsten/submit/forms.py
@@ -2,13 +2,15 @@
 from __future__ import unicode_literals
 from django import forms
 from django.conf import settings
+from django.utils.translation import ugettext_lazy
 
 import os
 
 
 class SourceSubmitForm(forms.Form):
     LANGUAGE_CHOICES = (
-        (".", "Zisti podľa prípony"),
+        # Translators: original: Zisti podľa prípony
+        (".", ugettext_lazy('Find out by extension')),
         (".cc", "C++ (.cpp/.cc)"),
         (".pas", "Pascal (.pas/.dpr)"),
         (".c", "C (.c)"),
@@ -21,7 +23,8 @@ class SourceSubmitForm(forms.Form):
         max_length=settings.UPLOADED_FILENAME_MAXLENGTH,
         allow_empty_file=True,
     )
-    language = forms.ChoiceField(label='Jazyk',
+    # Translators: original: Jazyk
+    language = forms.ChoiceField(label=ugettext_lazy('Language'),
                                  choices=LANGUAGE_CHOICES)
 
 class DescriptionSubmitForm(forms.Form):
@@ -34,7 +37,8 @@ class DescriptionSubmitForm(forms.Form):
         sfile = self.cleaned_data['submit_file']
         extension = os.path.splitext(sfile.name)[1]
         if extension.lower() not in settings.SUBMIT_DESCRIPTION_ALLOWED_EXTENSIONS:
-            raise forms.ValidationError("Zaslaný súbor má nepodporovanú príponu %s" % extension)
+            # Translators: original: Zaslaný súbor má nepodporovanú príponu %s
+            raise forms.ValidationError(ugettext_lazy("The submitted file has unsupported extension %s") % extension)
         return sfile
 
 class TestableZipSubmitForm(forms.Form):
@@ -47,6 +51,8 @@ class TestableZipSubmitForm(forms.Form):
         sfile = self.cleaned_data['submit_file']
         if sfile:
             if sfile.name.split('.')[-1].lower() != 'zip':
-                raise forms.ValidationError("Zaslaný súbor nemá koncovku .zip")
+                # Translators: original: Zaslaný súbor nemá koncovku .zip
+                raise forms.ValidationError(ugettext_lazy("The submitted file does not have the extension .zip"))
         else:
-            raise forms.ValidationError("Chýba súbor")
+            # Translators: original: Chýba súbor
+            raise forms.ValidationError(ugettext_lazy("File is missing"))
