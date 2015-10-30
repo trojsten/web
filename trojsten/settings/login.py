cd4dd015e70b57227e950456c43a59eb47d6f822
Michal Hozza <mhozza@gmail.com>
2015-10-30 21:33:36 +0100

    whitelisted urls

--- trojsten/settings/login.py
+++ trojsten/settings/login.py
@@ -5,3 +5,10 @@ SITE_ID = 10
 NAVBAR_SITES = []
 
 ROOT_URLCONF = 'trojsten.urls.login'
+
+CORS_ORIGIN_REGEX_WHITELIST = (
+    '^(https?://)?(\w+\.)*ksp\.sk$',
+    '^(https?://)?(\w+\.)*fks\.sk$',
+    '^(https?://)?(\w+\.)*kms\.sk$',
+    '^(https?://)?(\w+\.)*trojsten\.sk$',
+)
