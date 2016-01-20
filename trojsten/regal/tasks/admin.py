88e5c13b9dc214f4c49e4bef78858d7def9de14f
Michal Hozza <mhozza@gmail.com>
2016-01-20 15:13:16 +0100

    Fixed duplicate objects in queryset

--- trojsten/regal/tasks/admin.py
+++ trojsten/regal/tasks/admin.py
@@ -122,7 +122,7 @@ class TaskAdmin(admin.ModelAdmin):
         cat_lst = Category.objects.filter(competition__organizers_group__in=user_groups)
         return super(TaskAdmin, self).get_queryset(request).filter(
             category__in=cat_lst
-        )
+        ).distinct()
 
 
 class SubmitAdmin(admin.ModelAdmin):
@@ -171,7 +171,7 @@ class SubmitAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         user_groups = request.user.groups.all()
         cat_lst = Category.objects.filter(competition__organizers_group__in=user_groups)
-        task_lst = Task.objects.filter(category__in=cat_lst)
+        task_lst = Task.objects.filter(category__in=cat_lst).distinct()
         return super(SubmitAdmin, self).get_queryset(request).filter(
             task__in=task_lst
         )
