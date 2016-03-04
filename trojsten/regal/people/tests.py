b433d2f0e9a4c9ea0ca5ce324367727a8e679edf
Michal Hozza <mhozza@gmail.com>
2016-03-04 23:04:58 +0100

    improved merge_users test

--- trojsten/regal/people/tests.py
+++ trojsten/regal/people/tests.py
@@ -2,9 +2,10 @@
 import random
 
 from django.test import TestCase
+from django.contrib.auth.models import Group
 
 from .helpers import get_similar_users, merge_users
-from .models import User, UserProperty, UserPropertyKey
+from .models import DuplicateUser, User, UserProperty, UserPropertyKey
 
 
 class UserMergeTests(TestCase):
@@ -28,15 +29,15 @@ class UserMergeTests(TestCase):
         user3 = self.create_random_user(first_name='Jozef', last_name='Novak')
         similar_users = get_similar_users(user)
         self.assertQuerysetEqual(
-            similar_users, [user2.pk, user3.pk], transform=lambda u: u.pk, ordered=False
+            similar_users, [user2.pk, user3.pk], transform=lambda obj: obj.pk, ordered=False
         )
         similar_users = get_similar_users(user2)
         self.assertQuerysetEqual(
-            similar_users, [user.pk, user3.pk], transform=lambda u: u.pk, ordered=False
+            similar_users, [user.pk, user3.pk], transform=lambda obj: obj.pk, ordered=False
         )
         similar_users = get_similar_users(user3)
         self.assertQuerysetEqual(
-            similar_users, [user.pk, user2.pk], transform=lambda u: u.pk, ordered=False
+            similar_users, [user.pk, user2.pk], transform=lambda obj: obj.pk, ordered=False
         )
         similar_users = get_similar_users(user1)
         self.assertQuerysetEqual(similar_users, [])
@@ -62,11 +63,11 @@ class UserMergeTests(TestCase):
             UserProperty.objects.create(user=target_user, key=mobil, value='+421908123456')
             UserProperty.objects.create(user=source_user, key=tricko, value='M')
             UserProperty.objects.create(user=source_user, key=topanka, value='42')
-            UserProperty.objects.create(user=source_user, key=mobil, value='+421908123456')
             UserProperty.objects.create(user=source_user, key=telefon, value='+421212345678')
             UserProperty.objects.create(user=source_user, key=op, value='EA000444')
             return target_user, source_user
 
+        # Test merging fields and user props
         target_user, source_user = create_users_to_merge()
         merge_users(target_user, source_user, ['graduation'], [tricko.pk, telefon.pk])
         self.assertEqual(target_user.last_name, 'Novak')
@@ -80,15 +81,38 @@ class UserMergeTests(TestCase):
         ):
             target_user.properties.get(key=op)
 
+        # Test merging fields, user props, and deleting user props
         target_user, source_user = create_users_to_merge()
-        merge_users(target_user, source_user, ['email'], [topanka.pk, op.pk])
+        merge_users(target_user, source_user, ['email'], [topanka.pk, op.pk, mobil.pk])
         self.assertEqual(target_user.last_name, 'Novak')
         self.assertEqual(target_user.email, 'jozef.novak@gmail.com')
         self.assertEqual(target_user.graduation, 2017)
-        self.assertEqual(target_user.properties.get(key=mobil).value, '+421908123456')
         self.assertEqual(target_user.properties.get(key=topanka).value, '42')
         self.assertEqual(target_user.properties.get(key=tricko).value, 'L')
         with self.assertRaisesMessage(
             UserProperty.DoesNotExist, 'UserProperty matching query does not exist.'
         ):
+            target_user.properties.get(key=mobil)
+        with self.assertRaisesMessage(
+            UserProperty.DoesNotExist, 'UserProperty matching query does not exist.'
+        ):
             target_user.properties.get(key=telefon)
+
+        # Test merging related fields
+        g1 = Group.objects.create(name='grp1')
+        g2 = Group.objects.create(name='grp2')
+        g3 = Group.objects.create(name='grp3')
+        target_user, source_user = create_users_to_merge()
+        target_user.groups.add(g1)
+        target_user.groups.add(g2)
+        source_user.groups.add(g2)
+        source_user.groups.add(g3)
+        du = DuplicateUser.objects.create(user=source_user)
+        merge_users(target_user, source_user, [], [])
+        self.assertEqual(target_user.duplicateuser.pk, du.pk)
+        self.assertQuerysetEqual(
+            target_user.groups.all(), [g1.pk, g2.pk, g3.pk], transform=lambda obj: obj.pk,
+            ordered=False,
+        )
+
+        # Test the source user is deleted
