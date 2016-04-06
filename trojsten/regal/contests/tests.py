37aad26690e5a0569981c70ac0cc1b3790e7b2c2
lukasbalaz <lukasbalaz7@gmail.com>
2016-04-06 16:21:35 +0200

    PDF filenames tests

---
+++ trojsten/regal/contests/tests.py
@@ -0,0 +1,17 @@
+from django.utils.translation import activate
+from django.test import TestCase
+
+from trojsten.regal.contests.models import Competition, Series, Round
+
+
+class RoundMethodTests(TestCase):
+    def test_get_pdf_name(self):
+        c = Competition.objects.create(name='ABCD')
+        s = Series.objects.create(year=47, competition=c, number=1)
+        r = Round.objects.create(number=3, series=s, visible=True, solutions_visible=True)
+        activate('en')
+        self.assertEqual(r.get_pdf_name(), u'ABCD-year47-round3-tasks.pdf')
+        self.assertEqual(r.get_pdf_name(True), u'ABCD-year47-round3-solutions.pdf')
+        activate('sk')
+        self.assertEqual(r.get_pdf_name(), u'ABCD-rocnik47-kolo3-zadania.pdf')
+        self.assertEqual(r.get_pdf_name(True), u'ABCD-rocnik47-kolo3-vzoraky.pdf')
