a73aff324d7a74a4f9d9c39326bfb58b50f59d6d
Michal Hozza <mhozza@gmail.com>
2016-04-15 19:43:25 +0200

    verifying result

--- trojsten/special/plugin_prask_2_4_1/urls.py
+++ trojsten/special/plugin_prask_2_4_1/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import task_view, answer_query, submit
+from .views import task_view, answer_query
 
 app_name = 'plugin_prask_2_4_1'
 
 urlpatterns = patterns(
-    url(r'^api/query/?$', answer_query, name='plugin_prask_2_4_1:answer_query'),
-    url(r'^api/submit/?$', submit, name='plugin_prask_2_4_1:submit'),
-    url(r'^$', task_view, name='plugin_prask_2_4_1:task_view'),
+    url(r'^api/query/?$', answer_query, name='answer_query'),
+    url(r'^$', task_view, name='task_view'),
 )
--- trojsten/special/plugin_prask_2_4_1/views.py
+++ trojsten/special/plugin_prask_2_4_1/views.py
@@ -1,22 +1,48 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.urlresolvers import reverse
 from django.http.response import JsonResponse
 
 from .tester import process_question, process_answer, POCET_PRVKOV
+from .forms import SubmitForm
 
 
 def task_view(request):
-    return render(request, 'plugin_prask_2_4_1/task_view.html')
+    if 'plugin_prask_2_4_1' not in request.session:
+        request.session['plugin_prask_2_4_1'] = dict()
+    task_session = request.session['plugin_prask_2_4_1']
+    if request.method == 'POST':
+        form = SubmitForm(request.POST)
+        if form.is_valid():
+            queries = task_session.get('questions', list())
+            selection = form.cleaned_data['selection']
+            points, message = process_answer(queries, selection)
+            if points:
+                pass  # @TODO: Submit!
+            task_session['best_points'] = 'Todo!'
+            task_session['last_points'] = points
+            messages.add_message(request, messages.SUCCESS if points else messages.ERROR, message)
+            return redirect(reverse('plugin_prask_2_4_1:task_view'))
+    else:
+        form = SubmitForm()
+
+    context = dict(
+        form=form, last_points=task_session.get('last_points', 0), best_points=task_session.get('best_points', 0)
+    )
+    return render(request, 'plugin_prask_2_4_1/task_view.html', context=context)
 
 
 def answer_query(request):
+    if 'plugin_prask_2_4_1' not in request.session:
+        request.session['plugin_prask_2_4_1'] = dict()
     data = dict()
-    queries = request.session.get('plugin_prask_2_4_1/questions', list())
+    queries = request.session['plugin_prask_2_4_1'].get('questions', list())
     if request.method == 'DELETE':
         queries = list()
-        request.session['plugin_prask_2_4_1/questions'] = queries
+        request.session['plugin_prask_2_4_1']['questions'] = queries
         data['status'] = 'Success'
         data['queries'] = queries
         return JsonResponse(data)
@@ -50,29 +76,8 @@ def answer_query(request):
         data['message'] = 'Nesprávne parametre'
         return JsonResponse(data)
     process_question(queries, a, b)
-    request.session['plugin_prask_2_4_1/questions'] = queries
+    request.session['plugin_prask_2_4_1']['questions'] = queries
     data['status'] = 'Success'
     data['queries'] = queries
     return JsonResponse(data)
 
-
-def submit(request):
-    data = dict()
-    queries = request.session.get('plugin_prask_2_4_1/questions', list())
-    if request.method != 'POST':
-        data['status'] = 'Error'
-        data['message'] = 'Invalid method'
-        return JsonResponse(data)
-    selection = request.POST.get('selection', None)
-    if selection is None:
-        data['status'] = 'Error'
-        data['message'] = 'Invalid parameters'
-        return JsonResponse(data)
-
-    points, message = process_answer(queries, selection)
-    data = {
-        'status': 'Success',
-        'points': points,
-        'message': message,
-    }
-    return JsonResponse(data)
