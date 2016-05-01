bc762b44d7f51e895ee56deafa70f01e67cc91c2
Michal Hozza <mhozza@gmail.com>
2016-05-01 12:20:37 +0200

    separate python3 and python2 requirements

--- build_requirements.sh
+++ build_requirements.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip-compile requirements.in
-pip-compile requirements.in requirements.devel.in -o requirements.devel.txt
+pip-compile requirements.in requirements2.in -o requirements.txt
+pip-compile requirements.in -o requirements3.txt
+pip-compile requirements.in requirements2.in requirements.devel.in -o requirements.devel.txt
