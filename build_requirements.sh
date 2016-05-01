827019a507b5e8c5d254cc282d8176c56ec9a326
Michal Hozza <mhozza@gmail.com>
2016-05-01 12:53:50 +0200

    removed tablib (no idea what was it for, doesn't seem to be used in project)

--- build_requirements.sh
+++ build_requirements.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 pip-compile requirements.in requirements2.in -o requirements.txt
-pip-compile requirements.in -o requirements3.txt
 pip-compile requirements.in requirements2.in requirements.devel.in -o requirements.devel.txt
