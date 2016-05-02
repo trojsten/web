365214f6d112cb7c50e7ff2b330d188cb11f206d
Michal Hozza <mhozza@gmail.com>
2016-05-02 18:55:16 +0200

    fixed urlconf to silent deprecation warnings

--- trojsten/special/plugin_prask_2_4_1/urls.py
+++ trojsten/special/plugin_prask_2_4_1/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
-from .views import task_view, answer_query
+from django.conf.urls import url
+
+from .views import answer_query, task_view
 
 app_name = 'plugin_prask_2_4_1'
 
-urlpatterns = patterns(
+urlpatterns = [
     url(r'^api/query/?$', answer_query, name='answer_query'),
     url(r'^$', task_view, name='task_view'),
-)
+]
