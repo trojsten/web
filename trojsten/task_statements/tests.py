e778ea7e233a67b198eb2df5ed28089ce87c9d30
Michal Hozza <mhozza@gmail.com>
2016-05-02 17:19:32 +0200

    added simple task statements tests

--- trojsten/task_statements/tests.py
+++ trojsten/task_statements/tests.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from trojsten.contests.models import Competition, Series, Round
@@ -20,6 +20,11 @@ class TaskListTests(TestCase):
                                           solutions_visible=True)
         self.url = reverse('task_list', kwargs={'round_id': self.round.id})
 
+    def test_invalid_round(self):
+        url = reverse('task_list', kwargs={'round_id': 47})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
     def test_no_tasks(self):
         response = self.client.get(self.url)
         # @ToDo: translations
@@ -27,6 +32,28 @@ class TaskListTests(TestCase):
 
     def test_visible_tasks(self):
         Task.objects.create(number=1, name='Test task', round=self.round)
+        response = self.client.get(self.url)
+        # @ToDo: translations
+        self.assertContains(response, 'Test task')
+
+
+class TaskStatementsTests(TestCase):
+    def setUp(self):
+        competition = Competition.objects.create(name='TestCompetition')
+        competition.sites.add(Site.objects.get(pk=settings.SITE_ID))
+        series = Series.objects.create(number=1, name='Test series', competition=competition,
+                                       year=1)
+        self.round = Round.objects.create(number=1, series=series, visible=True,
+                                          solutions_visible=True)
+        self.task = Task.objects.create(number=1, name='Test task', round=self.round)
+        self.url = reverse('task_statement', kwargs={'task_id': self.task.id})
+
+    def test_invalid_task(self):
+        url = reverse('task_statement', kwargs={'task_id': 47})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_task_statement(self):
         response = self.client.get(self.url)
         # @ToDo: translations
         self.assertContains(response, 'Test task')
