7ea83fda584299d29c9a55426654a5e4bc59471d
Michal Hozza <mhozza@gmail.com>
2016-05-04 11:06:27 +0200

    more advenced tests for task statements

--- trojsten/task_statements/tests.py
+++ trojsten/task_statements/tests.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from os import path
+
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+from django.test import TestCase, override_settings, modify_settings
 
-from trojsten.contests.models import Competition, Series, Round
+from trojsten.contests.models import Competition, Round, Series
 from trojsten.tasks.models import Task
 
 
@@ -37,6 +39,40 @@ class TaskListTests(TestCase):
         self.assertContains(response, 'Test task')
 
 
+@override_settings(
+    TASK_STATEMENTS_PATH=path.join(path.dirname(__file__), 'test_data', 'statements'),
+    TASK_STATEMENTS_SUFFIX_YEAR='',
+    TASK_STATEMENTS_SUFFIX_ROUND='',
+    TASK_STATEMENTS_TASKS_DIR='tasks',
+    TASK_STATEMENTS_PREFIX_TASK='',
+    TASK_STATEMENTS_SOLUTIONS_DIR='solutions',
+    TASK_STATEMENTS_HTML_DIR='html',
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.contrib.auth.context_processors.auth',
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.i18n',
+                    'django.template.context_processors.media',
+                    'django.template.context_processors.static',
+                    'django.template.context_processors.tz',
+                    'django.template.context_processors.request',
+                    'django.contrib.messages.context_processors.messages',
+                    'sekizai.context_processors.sekizai',
+                    'ksp_login.context_processors.login_providers_both',
+                    'trojsten.context_processors.current_site',
+                    'trojsten.context_processors.version_string',
+                ],
+                'allowed_include_roots': [
+                    path.join(path.dirname(__file__), 'test_data', 'statements'),
+                ],
+            }
+        },
+    ]
+)
 class TaskStatementsTests(TestCase):
     def setUp(self):
         competition = Competition.objects.create(name='TestCompetition')
@@ -55,10 +91,44 @@ class TaskStatementsTests(TestCase):
 
     def test_task_statement(self):
         response = self.client.get(self.url)
-        # @ToDo: translations
         self.assertContains(response, 'Test task')
+        self.assertContains(response, 'test html task statement')
 
 
+@override_settings(
+    TASK_STATEMENTS_PATH=path.join(path.dirname(__file__), 'test_data', 'statements'),
+    TASK_STATEMENTS_SUFFIX_YEAR='',
+    TASK_STATEMENTS_SUFFIX_ROUND='',
+    TASK_STATEMENTS_TASKS_DIR='tasks',
+    TASK_STATEMENTS_PREFIX_TASK='',
+    TASK_STATEMENTS_SOLUTIONS_DIR='solutions',
+    TASK_STATEMENTS_HTML_DIR='html',
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.contrib.auth.context_processors.auth',
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.i18n',
+                    'django.template.context_processors.media',
+                    'django.template.context_processors.static',
+                    'django.template.context_processors.tz',
+                    'django.template.context_processors.request',
+                    'django.contrib.messages.context_processors.messages',
+                    'sekizai.context_processors.sekizai',
+                    'ksp_login.context_processors.login_providers_both',
+                    'trojsten.context_processors.current_site',
+                    'trojsten.context_processors.version_string',
+                ],
+                'allowed_include_roots': [
+                    path.join(path.dirname(__file__), 'test_data', 'statements'),
+                ],
+            }
+        },
+    ]
+)
 class SolutionStatementsTests(TestCase):
     def setUp(self):
         competition = Competition.objects.create(name='TestCompetition')
@@ -77,5 +147,48 @@ class SolutionStatementsTests(TestCase):
 
     def test_solution_statement(self):
         response = self.client.get(self.url)
-        # @ToDo: translations
         self.assertContains(response, 'Test task')
+        self.assertContains(response, 'test html solution statement')
+        self.assertContains(response, 'test html task statement')
+
+
+@override_settings(
+    TASK_STATEMENTS_PATH=path.join(path.dirname(__file__), 'test_data', 'statements'),
+    TASK_STATEMENTS_SUFFIX_YEAR='',
+    TASK_STATEMENTS_SUFFIX_ROUND='',
+    TASK_STATEMENTS_TASKS_DIR='tasks',
+    TASK_STATEMENTS_PREFIX_TASK='',
+    TASK_STATEMENTS_SOLUTIONS_DIR='solutions',
+    TASK_STATEMENTS_PDF='tasks.pdf',
+    TASK_STATEMENTS_SOLUTIONS_PDF='solutions.pdf',
+)
+class PdfDownloadTests(TestCase):
+    def setUp(self):
+        competition = Competition.objects.create(name='TestCompetition')
+        competition.sites.add(Site.objects.get(pk=settings.SITE_ID))
+        series = Series.objects.create(
+            number=1, name='Test series', competition=competition, year=1,
+        )
+        self.round = Round.objects.create(
+            number=1, series=series, visible=True, solutions_visible=True,
+        )
+
+    def test_invalid_task_pdf(self):
+        url = reverse('view_pdf', kwargs={'round_id': 47})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_invalid_solutions_pdf(self):
+        url = reverse('view_solutions_pdf', kwargs={'round_id': 47})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_task_pdf(self):
+        url = reverse('view_pdf', kwargs={'round_id': self.round.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_solution_pdf(self):
+        url = reverse('view_solutions_pdf', kwargs={'round_id': self.round.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
