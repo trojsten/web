9cecb5f385c476c62049d7dbbb20de974b42e1a9
Marián Horňák <marian.sysel.hornak@gmail.com>
2016-05-07 12:50:43 +0200

    Added explicit backlinks in Prask 2 4 3

--- trojsten/special/plugin_prask_2_4_3/views.py
+++ trojsten/special/plugin_prask_2_4_3/views.py
@@ -13,6 +13,13 @@ def solved(request, level):
     if created:
         update_points(user)
 
+    backlink = {
+        1: 'https://ksp.sk/~prask/specialne/2/4/3/#/f9cbeabf3adf',
+        2: 'https://ksp.sk/~prask/specialne/2/4/3/#/50ed1a145675',
+        3: 'https://ksp.sk/~prask/specialne/2/4/3/#/03f76fd7d802',
+    }.get(level, '#')
+
     return render(request, 'plugin_prask_2_4_3/level.html', {
         "created": created,
+        "backlink": backlink,
     }, current_app=request.resolver_match.namespace)
