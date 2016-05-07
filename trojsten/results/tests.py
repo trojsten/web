efb0e226cd07ed1ebdc0b2f519d4e775253b9c64
Barbora Klembarova <basa.akrobat@gmail.com>
2016-05-07 12:55:49 +0200

    testovanie single_round a show_staff

--- trojsten/results/tests.py
+++ trojsten/results/tests.py
@@ -21,8 +21,9 @@ class RecentResultsTest(TestCase):
         self.series = Series.objects.create(number=1, name='Test series', competition=competition,
                                             year=1)
         self.url = reverse('view_latest_results')
+        year = datetime.datetime.now().year + 2
         self.user = User.objects.create(username="TestUser", password="password",
-                                        first_name="Jozko", last_name="Mrkvicka", graduation=2047)
+                                        first_name="Jozko", last_name="Mrkvicka", graduation=year)
 
     def test_no_rounds(self):
         response = self.client.get(self.url)
@@ -30,25 +31,33 @@ class RecentResultsTest(TestCase):
         # @ToDo: translations
         self.assertContains(response, 'Ešte nebeží žiadne kolo.')
 
-    def test_two_rounds(self):
-        start1 = datetime.datetime.strptime('13 7 2013', '%d %m %Y')
-        end1 = datetime.datetime.strptime('13 8 2013', '%d %m %Y')
-        start2 = datetime.datetime.strptime('13 9 2013', '%d %m %Y')
-        end2 = datetime.datetime.strptime('13 10 2013', '%d %m %Y')
-        submit_time = datetime.datetime.strptime('12 8 2013', '%d %m %Y')
+    def test_rounds(self):
+        start1 = datetime.datetime.now() + datetime.timedelta(-8)
+        end1 = datetime.datetime.now() + datetime.timedelta(-4)
+        start2 = datetime.datetime.now() + datetime.timedelta(-4)
+        end2 = datetime.datetime.now() + datetime.timedelta(4)
         round1 = Round.objects.create(number=1, series=self.series, visible=True,
                                       solutions_visible=True, start_time=start1, end_time=end1)
-        Round.objects.create(number=1, series=self.series, visible=True, solutions_visible=True,
-                             start_time=start2, end_time=end2)
+        round2 = Round.objects.create(number=2, series=self.series, visible=True, start_time=start2,
+                                      end_time=end2, solutions_visible=True)
         task1 = Task.objects.create(number=1, name='Test task 1', round=round1)
+        task2 = Task.objects.create(number=1, name='Test task 2', round=round2)
+
         submit = Submit.objects.create(task=task1, user=self.user, submit_type=0, points=5)
-        submit.time = submit_time
+        submit.time = start1 + datetime.timedelta(0, 5)
         submit.save()
 
-        response = self.client.get(self.url)
+        response = self.client.get("%s?single_round=True" % self.url)
         self.assertNotContains(response, self.user.get_full_name())
         # @ToDo: translations
         self.assertContains(response, 'Žiadne submity')
+
+        submit = Submit.objects.create(task=task2, user=self.user, submit_type=0, points=5)
+        submit.time = start2 + datetime.timedelta(0, 5)
+        submit.save()
+
+        response = self.client.get("%s?single_round=True" % self.url)
+        self.assertContains(response, self.user.get_full_name())
 
     def test_closed_round(self):
         start = datetime.datetime.now() + datetime.timedelta(-8)
@@ -59,6 +68,25 @@ class RecentResultsTest(TestCase):
         response = self.client.get(self.url)
         # @ToDo: translations
         self.assertContains(response, 'Výsledky sú predbežné.')
+
+    def test_old_user(self):
+        old_user = User.objects.create(username="TestOldUser", password="password",
+                                       first_name="Jozko", last_name="Starcek", graduation=2010)
+        start = datetime.datetime.now() + datetime.timedelta(-4)
+        end = datetime.datetime.now() + datetime.timedelta(4)
+        test_round = Round.objects.create(number=1, series=self.series, visible=True,
+                                          solutions_visible=True, start_time=start, end_time=end)
+        task = Task.objects.create(number=1, name='Test task 1', round=test_round)
+
+        submit = Submit.objects.create(task=task, user=old_user, submit_type=0, points=5)
+        submit.time = start + datetime.timedelta(0, 5)
+        submit.save()
+
+        response = self.client.get(self.url)
+        self.assertNotContains(response, old_user.get_full_name())
+
+        response = self.client.get("%s?show_staff=True" % self.url)
+        self.assertContains(response, old_user.get_full_name())
 
 
 class ResultsTest(TestCase):
@@ -85,8 +113,10 @@ class ResultsTest(TestCase):
         self.task1 = Task.objects.create(number=1, name='Test task 1', round=self.round1)
         self.task2 = Task.objects.create(number=1, name='Test task 2', round=self.round2)
         self.task3 = Task.objects.create(number=1, name='Test task 3', round=self.round3)
+
+        year = datetime.datetime.now().year + 2
         self.user = User.objects.create(username="TestUser", password="password",
-                                        first_name="Jozko", last_name="Mrkvicka", graduation=2047)
+                                        first_name="Jozko", last_name="Mrkvicka", graduation=year)
         self.url1 = reverse('view_results', kwargs={'round_id': self.round1.id})
         self.url2 = reverse('view_results', kwargs={'round_id': self.round2.id})
         self.url3 = reverse('view_results', kwargs={'round_id': self.round3.id})
@@ -146,4 +176,31 @@ class ResultsTest(TestCase):
         response = self.client.get(self.url2)
         self.assertContains(response, self.user.get_full_name())
         response = self.client.get(self.url3)
+        self.assertContains(response, self.user.get_full_name())
+
+    def test_old_user(self):
+        old_user = User.objects.create(username="TestOldUser", password="password",
+                                       first_name="Jozko", last_name="Starcek", graduation=2010)
+        submit = Submit.objects.create(task=self.task1, user=old_user, submit_type=0, points=5)
+        submit.time = self.round1.start_time + datetime.timedelta(0, 5)
+        submit.save()
+
+        response = self.client.get(self.url1)
+        self.assertNotContains(response, old_user.get_full_name())
+
+        response = self.client.get("%s?show_staff=True" % self.url1)
+        self.assertContains(response, old_user.get_full_name())
+
+    def test_single_round(self):
+        submit_time = self.round1.start_time + datetime.timedelta(0, 5)
+        submit = Submit.objects.create(task=self.task1, user=self.user, submit_type=0, points=5)
+        submit.time = submit_time
+        submit.save()
+        response = self.client.get("%s?single_round=True" % self.url2)
+        self.assertNotContains(response, self.user.get_full_name())
+
+        submit = Submit.objects.create(task=self.task2, user=self.user, submit_type=0, points=5)
+        submit.time = self.round2.start_time + datetime.timedelta(0, 5)
+        submit.save()
+        response = self.client.get("%s?single_round=True" % self.url2)
         self.assertContains(response, self.user.get_full_name())
