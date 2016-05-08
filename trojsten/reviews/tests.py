83f11926d31195ddafc7085f02f2d38251c3404a
Barbora Klembarova <basa.akrobat@gmail.com>
2016-05-08 16:39:22 +0200

    testy view task_pk/review

--- trojsten/reviews/tests.py
+++ trojsten/reviews/tests.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import sys
 try:
     from urllib import quote, unquote
 except:
     from urllib.request import quote, unquote
 
+import datetime
+
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from trojsten.contests.models import Competition, Round, Series
+from trojsten.tasks.models import Submit
+from trojsten.tasks.models import Task
 from trojsten.reviews.forms import ZipForm
 from trojsten.people.models import User
+from trojsten.utils.test_utils import get_noexisting_id
 
+from trojsten.submit.constants import SUBMIT_STATUS_REVIEWED
 
 class ReviewZipFormTests(TestCase):
     def setUp(self):
@@ -67,3 +79,99 @@ class ReviewZipFormTests(TestCase):
 
         self.assertEqual(z.initial['filename'], quote(self.test_str_utf8))
         self.assertEqual(z.initial['comment'], self.test_str)
+
+
+class ReviewTest(TestCase):
+    def setUp(self):
+        year = datetime.datetime.now().year + 2
+        self.user = User.objects.create_user(username="TestUser", password="password",
+                                             first_name="Jozko", last_name="Mrkvicka",
+                                             graduation=year)
+        self.staff = User.objects.create_user(username="TestStaff", password="password",
+                                              first_name="Jozko", last_name="Veduci",
+                                              graduation=2014)
+        self.staff.is_staff = True
+        self.staff.save()
+
+        group = Group.objects.create(name="Test Group")
+        group.user_set.add(self.staff)
+        competition = Competition.objects.create(name='TestCompetition', organizers_group=group)
+        competition.sites.add(Site.objects.get(pk=settings.SITE_ID))
+        self.series = Series.objects.create(number=1, name='Test series 1', year=1,
+                                            competition=competition)
+
+        start = datetime.datetime.now() + datetime.timedelta(-8)
+        end = datetime.datetime.now() + datetime.timedelta(-4)
+        self.round = Round.objects.create(number=1, series=self.series, solutions_visible=True,
+                                          start_time=start, end_time=end, visible=True)
+        self.no_submit_task = Task.objects.create(number=1, name='Test task 1', round=self.round)
+        self.task = Task.objects.create(number=2, name='Test task 2', round=self.round)
+        self.submit = Submit.objects.create(task=self.task, user=self.user, submit_type=1, points=5)
+        self.submit.time = self.task.round.start_time + datetime.timedelta(0, 5)
+        self.submit.save()
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_redirect_to_login(self):
+        url = reverse('admin:review_task', kwargs={'task_pk': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+        self.client.force_login(self.user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_invalid_task(self):
+        self.client.force_login(self.staff)
+        url = reverse('admin:review_task', kwargs={'task_pk': get_noexisting_id(Task)})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_staff_not_in_group(self):
+        staff = User.objects.create_user(username="TestStaffOther", password="password",
+                                         first_name="Jozko", last_name="Veduci",
+                                         graduation=2014)
+        staff.is_staff = True
+        staff.save()
+        self.client.force_login(staff)
+        url = reverse('admin:review_task', kwargs={'task_pk': self.no_submit_task.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_valid_task(self):
+        self.client.force_login(self.staff)
+        url = reverse('admin:review_task', kwargs={'task_pk': self.no_submit_task.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.no_submit_task.name)
+
+    def test_not_reviewable_submit(self):
+        for s_type in [0, 2, 3]:
+            submit = Submit.objects.create(task=self.no_submit_task, user=self.user,
+                                           submit_type=s_type, points=5)
+            submit.time = self.no_submit_task.round.start_time + datetime.timedelta(0, 5)
+            submit.save()
+        self.client.force_login(self.staff)
+        url = reverse('admin:review_task', kwargs={'task_pk': self.no_submit_task.id})
+        response = self.client.get(url)
+        self.assertNotContains(response, self.user.get_full_name())
+
+    def test_description_submit(self):
+        self.client.force_login(self.staff)
+        url = reverse('admin:review_task', kwargs={'task_pk': self.task.id})
+        response = self.client.get(url)
+        self.assertContains(response, self.user.get_full_name())
+
+    def test_reviewed(self):
+        comment = "TESTINGcomment"
+        self.client.force_login(self.staff)
+        url = reverse('admin:review_task', kwargs={'task_pk': self.task.id})
+        response = self.client.get(url)
+        self.assertNotContains(response, comment)
+
+        self.submit.testing_status = SUBMIT_STATUS_REVIEWED
+        self.submit.reviewer_comment = "TESTINGcomment"
+        self.submit.save()
+        response = self.client.get(url)
+        self.assertContains(response, comment)
