9a4306897f25bf300d4f3b2ba5e786c6ae964d0c
Barbora Klembarova <basa.akrobat@gmail.com>
2016-05-10 00:34:46 +0200

    magicka konstanta paginate_by presunuta do samostatneho suboru

--- trojsten/news/views.py
+++ trojsten/news/views.py
@@ -2,13 +2,14 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.views.generic import ListView
 
 from trojsten.news.models import Entry
+from trojsten.news.constants import NEWS_PAGINATE_BY
 
 
 class EntryListView(ListView):
     model = Entry
     template_name = 'trojsten/news/index.html'
     context_object_name = 'news_entries'
-    paginate_by = 10
+    paginate_by = NEWS_PAGINATE_BY
 
     def get_queryset(self):
         return self.model.objects.filter(
