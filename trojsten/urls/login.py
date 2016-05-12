7eeb3723641db5d95cb99fae7478aeb169d98d60
Michal Hozza <mhozza@gmail.com>
2016-05-12 14:23:42 +0200

    added admin urls for simpler testing

--- trojsten/urls/login.py
+++ trojsten/urls/login.py
@@ -5,7 +5,8 @@ from trojsten.login.views import login_root_view
 from .common import *
 
 urlpatterns += [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^ucet/', include('ksp_login.urls')),
-    url(r'', login_root_view)
+    url(r'^$', login_root_view)
 ]
