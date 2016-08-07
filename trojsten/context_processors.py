cb92c19f7b276eef1ffafc52201703214ab4b864
Michal Hozza <mhozza@gmail.com>
2016-08-07 13:14:27 +0100

    correct login provider settings url

--- trojsten/context_processors.py
+++ trojsten/context_processors.py
@@ -7,6 +7,7 @@ def current_site(request):
     return {
         'SITE': settings.SITES[settings.SITE_ID],
         'OTHER_SITES': [settings.SITES[k] for k in settings.NAVBAR_SITES],
+        'TROJSTEN_LOGIN_PROVIDER_URL': settings.TROJSTEN_LOGIN_PROVIDER_URL,
     }
 
 
