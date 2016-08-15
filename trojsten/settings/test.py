ba180d8e4255073715dc74ffd7aa4ff0f0d432e3
Michal Hozza <mhozza@mhozza-macbookair.roam.corp.google.com>
2016-08-14 20:15:00 -0700

    test settings now works without configuration

--- trojsten/settings/test.py
+++ trojsten/settings/test.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
-from trojsten.settings.production import *
+from trojsten.settings.common import *
 
+DEBUG = False
 SENDFILE_BACKEND = 'sendfile.backends.development'
