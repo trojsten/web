c1cf6bdc7691b6ea2594e0eca9b1522dbad63f0d
dodo42 <j.rajnik@gmial.com>
2016-08-18 13:53:29 +0200

    less db queries

--- trojsten/search_indexes.py
+++ trojsten/search_indexes.py
@@ -7,12 +7,19 @@ from wiki.models import Article
 
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    #modified = indexes.DateField(model_attr='modified')
-    #title = indexes.CharField(model_attr='current_revision.title')
-    #content = indexes.CharField(model_attr='current_revision.content')
+    modified = indexes.DateTimeField(model_attr='modified')
+    title = indexes.CharField(model_attr='current_revision__title')
+    content = indexes.CharField(model_attr='current_revision__content')
+    deleted = indexes.BooleanField(model_attr='current_revision__deleted')
+    locked = indexes.BooleanField(model_attr='current_revision__locked')
+    urlpath = indexes.CharField(model_attr='get_absolute_url')
+    id = indexes.IntegerField(model_attr='id')
 
     def get_model(self):
         return Article
+
+    def prepare_urlpath(self, object):
+        return object.get_absolute_url()
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
