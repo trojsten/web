d32ec548e5ee37d26d1694a1113f77458f273ec8
Roman Hudec <roman@ksp.sk>
2016-10-25 20:28:25 +0200

    update build_requirements.sh for python3 (#922)

--- build_requirements.sh
+++ build_requirements.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-pip-compile requirements.in requirements2.in -o requirements.txt
-pip-compile requirements.in requirements2.in requirements.devel.in -o requirements.devel.txt
+
+version=`python -c "import sys; print(sys.version)" | head -n1 | cut -c1`
+if [ $version = 3 ]
+then
+    SOURCES="requirements.in"
+else
+    SOURCES="requirements.in requirements2.in"
+fi
+
+pip-compile $SOURCES -o requirements.txt
+pip-compile $SOURCES requirements.devel.in -o requirements.devel.txt
