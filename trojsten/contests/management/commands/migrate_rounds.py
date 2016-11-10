24824f30a3d2fd11f73aa11689dc89f44d1bff57
Michal Hozza <mhozza@gmail.com>
2016-11-10 16:34:18 +0000

    changed filesystem structure for storing tasks (#891)
    * changed filesystem structure for storing tasks
    
    * simple round migration management command
    
    * fixed review comments

---
+++ trojsten/contests/management/commands/migrate_rounds.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import os
+import six
+import shutil
+from glob import glob
+
+from django.utils.six.moves import input
+from django.conf import settings
+from django.core.management.base import NoArgsCommand
+
+from trojsten.contests.models import Competition, Round, Semester
+
+
+class Command(NoArgsCommand):
+    help = 'Migrates contest directory structure.'
+
+    def handle_noargs(self, **options):
+        # Compute changes
+        move_paths = list()
+        rounds = list()
+
+        for competition in glob(os.path.join(settings.TASK_STATEMENTS_PATH, '*')):
+            for year in glob(os.path.join(competition, '*rocnik')):
+                for rnd in glob(os.path.join(year, '*kolo')):
+                    new_path, round_obj = self.migrate_path(rnd)
+                    move_paths.append((rnd, new_path))
+                    if round_obj:
+                        rounds.append(round_obj)
+
+        # List changes
+        self.stdout.write('Following paths are going to be moved:')
+        self.stdout.write('\n'.join(('%s -> %s' % move for move in move_paths)))
+        self.stdout.write('Following rounds are going to be renumbered:')
+        self.stdout.write('\n'.join((six.text_type(r) for r in rounds)))
+        # Ask and apply
+        choice = input('Do you wish to proceed? [yN]: ')
+        if choice and choice[0].lower() == 'y':
+            for src, dst in move_paths:
+                shutil.move(src, dst)
+            for round in rounds:
+                round.save()
+
+    def migrate_path(self, old_path):
+        prefix, competition, year, round = old_path.rsplit('/', 3)
+        year = ''.join(year[:-len('rocnik')])
+        round = ''.join(round[:-len('kolo')])
+        split = 3 if competition in {'UFO', 'FKS'} else 2
+        semester = '1' if int(round) <= split else '2'
+
+        round_obj = None
+        if semester == '2':
+            new_round = str(int(round) - split)
+            try:
+                competition_obj = Competition.objects.get(name=competition)
+                semester_obj = Semester.objects.get(competition=competition_obj, year=year, number=semester)
+                round_obj = Round.objects.get(number=round, semester=semester_obj)
+                round_obj.number = int(new_round)
+            except (Competition.DoesNotExist, Semester.DoesNotExist, Round.DoesNotExist) as e:
+                print('%s not in DB: %s' % (old_path, e))
+            round = new_round
+
+        return os.path.join(prefix, competition, year, semester, round), round_obj
