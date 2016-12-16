ff6d95d689b9fc55c7e3d17194fe7ff5ababeacb
Michal Hozza <mhozza@gmail.com>
2016-12-16 20:13:57 +0000

    using reply_to

--- trojsten/contact_form/forms.py
+++ trojsten/contact_form/forms.py
@@ -1,6 +1,7 @@
 from __future__ import  unicode_literals
 
 from django import forms
+from django.core.mail import EmailMessage
 from django.utils.translation import ugettext_lazy as _
 
 from contact_form import forms as contact_forms
@@ -10,10 +11,45 @@ class ContactForm(contact_forms.ContactForm):
     subject = forms.CharField(max_length=100,
                               label=_('Subject'))
 
-    def from_email(self):
-        return self.cleaned_data['email']
-
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
+        self.to = self.recipient_list
+
+    def body(self):
+        return self.message()
+
+    def reply_to(self):
+        return [self.cleaned_data['email']]
+
+    def get_message_dict(self):
+        """
+        Generate the various parts of the message and return them in a
+        dictionary, suitable for passing directly as keyword arguments
+        to ``django.core.mail.send_mail()``.
+
+        By default, the following values are returned:
+
+        * ``from_email``
+
+        * ``body``
+
+        * ``to``
+
+        * ``subject``
+
+        """
+        if not self.is_valid():
+            raise ValueError(
+                "Message cannot be sent from invalid contact form"
+            )
+        message_dict = {}
+        for message_part in ('from_email', 'body',
+                             'to', 'subject', 'reply_to'):
+            attr = getattr(self, message_part)
+            message_dict[message_part] = attr() if callable(attr) else attr
+        return message_dict
+
+    def save(self, fail_silently=False):
+        EmailMessage(**self.get_message_dict()).send(fail_silently=fail_silently)
 
 
