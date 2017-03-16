1069a50e4ae7134da46d85e00fc13b31656f215d
Michal Hozza <mhozza@gmail.com>
2017-03-16 20:57:54 +0000

    Implemented captcha in contact form. (#1017)
    * Implemented captcha in contact form.
    Only for anonymous users.
    
    * captcha test
    
    * disable captcha in dev env
    
    * added view tests

--- trojsten/contact_form/forms.py
+++ trojsten/contact_form/forms.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
 
+from contact_form import forms as contact_forms
 from django import forms
 from django.core.mail import EmailMessage
 from django.utils.translation import ugettext_lazy as _
-
-from contact_form import forms as contact_forms
+from snowpenguin.django.recaptcha2.fields import ReCaptchaField
+from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
 
 class ContactForm(contact_forms.ContactForm):
@@ -14,12 +15,14 @@ class ContactForm(contact_forms.ContactForm):
 
     field_order = ['name', 'email', 'subject', 'body']
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, captcha, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.to = self.recipient_list
         self.fields['name'].label = _('Name')
         self.fields['email'].label = _('Email')
         self.fields['body'].label = _('Message')
+        if captcha:
+            self.fields['captcha'] = ReCaptchaField(widget=ReCaptchaWidget())
 
     def body(self):
         return self.message()
