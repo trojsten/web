0c27decd64b532e905c3ac61ef9316bb44b63be7
Marián Horňák <marian.sysel.hornak@gmail.com>
2017-04-28 19:13:09 +0200

    Prask plugin 3 3 3

---
+++ trojsten/special/plugin_prask_3_3_3/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from .views import solved
+
+urlpatterns = [
+    url(r'^1/nasiel_si_velmi_kratky_cyklus/', solved, {'level': 1}),
+    url(r'^2/int_ti_obcas_pretecie/', solved, {'level': 2}),
+    url(r'^3/da_sa_to_aj_bruteforcit/', solved, {'level': 3}),
+    url(r'^4/moznosti_na_seedy_neni_az_tak_vela/', solved, {'level': 4}),
+]
--- trojsten/special/urls.py
+++ trojsten/special/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^prask/2/3/3/', include('trojsten.special.plugin_prask_2_3_3.urls')),
     url(r'^prask/2/4/1/', include('trojsten.special.plugin_prask_2_4_1.urls')),
     url(r'^prask/2/4/3/', include('trojsten.special.plugin_prask_2_4_3.urls')),
+    url(r'^prask/3/3/3/', include('trojsten.special.plugin_prask_3_3_3.urls')),
 ]
