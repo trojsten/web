8e40d320b64b791c932740328f1fe836b4d8b737
maaario <mlipovskysk@gmail.com>
2017-09-16 17:31:26 +0200

    use six.text_type instead of builtins.str for python 2-3 compatibility

--- trojsten/rules/management/commands/ksp_recalculate_levels.py
+++ trojsten/rules/management/commands/ksp_recalculate_levels.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from builtins import str
-
 from django.core.management.base import BaseCommand
+from django.utils.six import text_type
 
 from trojsten.rules.models import KSPLevel
 from trojsten.rules.ksp_levels import prepare_events, level_updates_from_camp_attendance, \
@@ -24,13 +23,13 @@ class Command(BaseCommand):
                 continue
             updates = list()
             if event.semester is not None:
-                self.stdout.write(str(event.semester))
+                self.stdout.write(text_type(event.semester))
                 updates = level_updates_from_semester_results(event.semester)
             elif event.camp is not None:
-                self.stdout.write(str(event.camp))
+                self.stdout.write(text_type(event.camp))
                 updates = level_updates_from_camp_attendance(event.camp, event.associated_semester,
                                                              event.last_semester_before_level_up)
-            self.stdout.write('\n'.join(map(str, updates)))
+            self.stdout.write('\n'.join(map(text_type, updates)))
 
             for update in updates:
                 update.save()
