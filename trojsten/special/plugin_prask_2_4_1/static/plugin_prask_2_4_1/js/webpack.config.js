4b2610f794b5f8a238cfcaeba9fe53caac7ad8ab
kubik369 <kubik0369@gmail.com>
2018-01-22 00:42:35 +0100

    Remove React from static scripts, fix Prask task accordingly

---
+++ trojsten/special/plugin_prask_2_4_1/static/plugin_prask_2_4_1/js/webpack.config.js
@@ -0,0 +1,32 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./src/main.jsx",
+  output: {
+    path: __dirname + "/lib",
+    filename: "main.min.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      sourcemap: false,
+      minimize: true
+    }),
+  ],
+};
