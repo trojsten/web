3efcb874cae557c3a021982b5a88d80bde4fb66b
kubik369 <kubik0369@gmail.com>
2018-01-21 22:14:03 +0100

    Add lazy loading carousel to home page

--- trojsten/static/js/menu.js
+++ trojsten/static/js/menu.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
-    $("body").on( "swiperight", function( event ) {
-        if(event.swipestart.coords[0] < $('body').width() * .1) {
-            $('.nav-menu:not(.offcanvas-clone):not(:visible)').offcanvas('show');
-        }
-    });
+  $('body').on('swiperight', function (event) {
+    if (event.swipestart.coords[0] < $('body').width() * .1) {
+      $('.nav-menu:not(.offcanvas-clone):not(:visible)').offcanvas('show');
+    }
+  });
 
-    $("body").on( "swipeleft", function( event ) {
-        $('.nav-menu.in:not(.offcanvas-clone)').offcanvas('hide');
-    });
+  $('body').on('swipeleft', function (event) {
+    $('.nav-menu.in:not(.offcanvas-clone)').offcanvas('hide');
+  });
 
-    $(".offcanvas-xs").on("show.bs.offcanvas", function(event) {
-        $(".navbar-toggle").addClass("active");
-    });
+  $('.offcanvas-xs').on('show.bs.offcanvas', function (event) {
+    $('.navbar-toggle').addClass('active');
+  });
 
-    $(".offcanvas-xs").on("hidden.bs.offcanvas", function(event) {
-        $(".navbar-toggle").removeClass("active");
-    });
+  $('.offcanvas-xs').on('hidden.bs.offcanvas', function (event) {
+    $('.navbar-toggle').removeClass('active');
+  });
+
+  $('.carousel.lazy').bind('slide.bs.carousel', function (event) {
+    const lazy = $(event.relatedTarget).find('img[data-src]');
+    lazy.attr('src', lazy.data('src'));
+    lazy.removeAttr('data-src');
+  });
 });
