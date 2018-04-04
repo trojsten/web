4e94e5aabb38e8e06b041df8cea55aa79c716b33
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2018-04-04 22:12:43 +0200

    Fixing PEP8 issuest

--- trojsten/results/views.py
+++ trojsten/results/views.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
 
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from trojsten.contests.models import Competition, Round
 from trojsten.utils.utils import is_true
 
 from .constants import DEFAULT_TAG_KEY
-from .manager import TagKeyError, get_results, get_results_tags_for_rounds
+from .manager import get_results, get_results_tags_for_rounds
 
 
 def view_results(request, round_id, tag_key=DEFAULT_TAG_KEY):
@@ -27,7 +26,8 @@ def view_results(request, round_id, tag_key=DEFAULT_TAG_KEY):
     single_round = is_true(request.GET.get('single_round', False))
 
     ResultTableObject = namedtuple(
-        'ResultTableObject', ['scoreboard', 'tag_name', 'competition_ignored', 'user_valid']
+        'ResultTableObject', ['scoreboard', 'tag_name',
+                              'competition_ignored', 'user_valid']
     )
 
     scoreboards = [
@@ -46,9 +46,9 @@ def view_results(request, round_id, tag_key=DEFAULT_TAG_KEY):
     ]
 
     context = {
-        'round_score' : scoreboards[0][0],
+        'round_score': scoreboards[0][0],
         'scoreboards': scoreboards,
-        'selected_tag' : tag_key,
+        'selected_tag': tag_key,
         'tag_name': round.semester.competition.rules.RESULTS_TAGS.get(tag_key).name,
         'show_staff': is_true(request.GET.get('show_staff', False)),
     }
@@ -75,7 +75,8 @@ def view_latest_results(request):
     single_round = is_true(request.GET.get('single_round', False))
 
     ResultTableObject = namedtuple(
-        'ResultTableObject', ['scoreboard', 'tag_name', 'competition_ignored', 'user_valid']
+        'ResultTableObject', ['scoreboard', 'tag_name',
+                              'competition_ignored', 'user_valid']
     )
 
     scoreboards = [
