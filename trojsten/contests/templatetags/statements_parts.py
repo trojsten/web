5ea0b4a3de704ea2ff308e9b0544e6de99a4e544
maaario <mlipovskysk@gmail.com>
2018-06-10 14:23:15 +0200

    add descriptive comment

--- trojsten/contests/templatetags/statements_parts.py
+++ trojsten/contests/templatetags/statements_parts.py
@@ -23,6 +23,7 @@ def show_task_list(context, round):
     ).select_related(
         'round', 'round__semester', 'round__semester__competition'
     )
+    # Select all categories which are represented by at least one task in displayed round.
     categories = Category.objects.filter(task__in=tasks.values_list('pk', flat=True)).distinct()
 
     data = {
