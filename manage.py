7817e93a133284b31793a6dfc6ee847a9f65f892
Michal Hozza <mhozza@gmail.com>
2018-07-08 22:53:40 +0100

    Removed invalid method call from manage.py

--- manage.py
+++ manage.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import os
+
 import sys
-import textwrap
 
 import dotenv
-from django.db import connection
-
+import os
 
 if __name__ == "__main__":
     dotenv.read_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trojsten.settings.development")
 
-    if 'migrate' in sys.argv:
-        rename_submit_app_in_db()
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
