b1a40e5a7d602d036e29e0211d8fd51182f51589
Emanuel Tesař <e.tesarr@gmail.com>
2018-10-01 23:23:07 +0200

    Implement Prask zwarte doos (#1187)
    Implement Prask zwarte doos 5.1.1

---
+++ trojsten/special/plugin_prask_5_1_1/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from trojsten.special.plugin_prask_5_1_1 import views
+
+urlpatterns = [
+    url(r'^$', views.main),
+    url(r'^level/(?P<level>\d+)/', views.main, name="main"),
+    url(r'^run/(?P<level>\d+)/', views.run, name="run"),
+]
