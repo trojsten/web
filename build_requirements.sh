3e4eb11fa43720b8e2a0cef2717658f7d9c5782c
Michal Hozza <mhozza@gmail.com>
2019-01-22 14:04:02 +0000

    Cleanup requirements.
    Removes py2 requirements and merges dev requirements into prod.

--- build_requirements.sh
+++ build_requirements.sh
@@ -4,11 +4,8 @@ version=`python -c "import sys; print(sys.version)" | head -n1 | cut -c1`
 if [ $version = 3 ]
 then
     SOURCES="requirements.in"
-    pip-compile -r $SOURCES -o requirements3.txt
-    pip-compile -r $SOURCES requirements.devel.in -o requirements3.devel.txt
-else
-    SOURCES="requirements.in requirements2.in"
     pip-compile -r $SOURCES -o requirements.txt
-    pip-compile -r $SOURCES requirements.devel.in -o requirements.devel.txt
+else
+    echo "Python 2 is no longer supported."
 fi
 
