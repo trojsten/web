c54396df69b8ddf43df34fb5289f9a70c34dba3e
Adam (ano95) <adam@ano95.eu>
2019-10-05 23:23:59 +0200

    Run black on newly generated migration.

--- trojsten/contests/migrations/0019_auto_20191005_2320.py
+++ trojsten/contests/migrations/0019_auto_20191005_2320.py
@@ -6,19 +6,25 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('contests', '0018_merge_20191005_2301'),
-    ]
+    dependencies = [("contests", "0018_merge_20191005_2301")]
 
     operations = [
         migrations.AlterField(
-            model_name='taskpeople',
-            name='role',
-            field=models.IntegerField(choices=[(0, 'opravovateľ'), (1, 'solution writer'), (2, 'recenzovač')], verbose_name='funkcia'),
+            model_name="taskpeople",
+            name="role",
+            field=models.IntegerField(
+                choices=[(0, "opravovateľ"), (1, "solution writer"), (2, "recenzovač")],
+                verbose_name="funkcia",
+            ),
         ),
         migrations.AlterField(
-            model_name='taskpeople',
-            name='task',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='task_people', to='contests.Task', verbose_name='task'),
+            model_name="taskpeople",
+            name="task",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="task_people",
+                to="contests.Task",
+                verbose_name="task",
+            ),
         ),
     ]
