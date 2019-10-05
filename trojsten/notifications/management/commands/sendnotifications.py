f1b53048005b6e4b69e63e5ccdbeb6a138a0d6b0
Adam (ano95) <adam@ano95.eu>
2019-10-05 22:52:25 +0200

    Add email sending command

---
+++ trojsten/notifications/management/commands/sendnotifications.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from trojsten.notifications.models import Notification
+from trojsten.people.models import User
+from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+
+class Command(BaseCommand):
+    help = 'Sends notification emails.'
+
+    def handle(self, *args, **options):
+        base_query = Notification.objects.filter(was_email_sent=False, was_read=False)
+        root_domain = Site.objects.get(pk=10).domain
+
+        for user in base_query.values('user_id').distinct():
+            user = User.objects.filter(pk=user['user_id']).get()
+            user_notifications = base_query.filter(user=user)
+
+            mail_message = render_to_string('trojsten/notifications/email.txt', {
+                'user': user,
+                'notifications': user_notifications,
+                'domain': root_domain
+            })
+
+            send_mail(
+                _('You have new notifications from Trojsten'),
+                mail_message,
+                settings.DEFAULT_FROM_EMAIL,
+                [user.email]
+            )
+
+            user_notifications.update(was_email_sent=True)
