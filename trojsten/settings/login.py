9c40524fab6f28f6fd7ece9aafbddf9e950c33ce
Michal Hozza <mhozza@gmail.com>
2019-11-29 18:05:15 +0000

    Move login provider settings to login  settings.

--- trojsten/settings/login.py
+++ trojsten/settings/login.py
@@ -29,3 +29,14 @@ SOCIAL_AUTH_PIPELINE = (
     "social_core.pipeline.social_auth.associate_user",
     "social_core.pipeline.social_auth.load_extra_data",
 )
+
+# Top secret keys for production settings
+SOCIAL_AUTH_FACEBOOK_KEY = requiredenv("TROJSTENWEB_FACEBOOK_KEY")
+SOCIAL_AUTH_FACEBOOK_SECRET = requiredenv("TROJSTENWEB_FACEBOOK_SECRET")
+SOCIAL_AUTH_FACEBOOK_SCOPE = ["email"]
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {"locale": "sk_SK", "fields": "id,name,email"}
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = requiredenv("TROJSTENWEB_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = requiredenv("TROJSTENWEB_GOOGLE_OAUTH2_SECRET")
+SOCIAL_AUTH_GITHUB_KEY = requiredenv("TROJSTENWEB_GITHUB_KEY")
+SOCIAL_AUTH_GITHUB_SECRET = requiredenv("TROJSTENWEB_GITHUB_SECRET")
+SOCIAL_AUTH_GITHUB_SCOPE = ["email"]
