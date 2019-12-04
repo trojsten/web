f75ce7ed12fe3340ef4c8d2db0c173ed9ead217c
Michal Hozza <mhozza@gmail.com>
2019-12-04 09:30:28 +0000

    Extract installing deps to shell scrpts.

---
+++ docker/common/install_runtime_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+apk add --update --no-cache --virtual runtime-deps \
+    freetype \
+    fribidi \
+    gettext \
+    harfbuzz \
+    jpeg \
+    lcms2 \
+    libpq \
+    musl \
+    openjpeg \
+    tcl \
+    tiff \
+    tk \
+    zlib
