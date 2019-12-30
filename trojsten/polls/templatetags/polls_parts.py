bc9262e40c4f00463713c4a41c8944b80dbd6538
Michal Stanik <michal.stanik@trojsten.sk>
2019-12-30 18:53:19 +0100

    Progress bar fixed
    Fixed progress bar which shows elapsed time of a poll

--- trojsten/polls/templatetags/polls_parts.py
+++ trojsten/polls/templatetags/polls_parts.py
@@ -9,8 +9,8 @@ from trojsten.polls.models import Question
 register = template.Library()
 
 
-@register.inclusion_tag("trojsten/polls/parts/progress.html")
-def show_time(current_question):
+@register.inclusion_tag("trojsten/polls/parts/progress.html", takes_context=True)
+def show_time(context, current_question):
     start = current_question.created_date
     end = current_question.deadline
     full = end - start
@@ -22,16 +22,20 @@ def show_time(current_question):
         progressbar_class = settings.ROUND_PROGRESS_WARNING_CLASS
     else:
         progressbar_class = settings.ROUND_PROGRESS_DEFAULT_CLASS
-    return {
+    percent = 100 * elapsed.days // full.days if full.days > 0 else 100
+    print("Percent is", percent, full.days, elapsed.days)
+    context.update( {
             "current": current_question,
             "start": start,
             "end": end,
             "full": full,
             "remaining": remaining,
             "elapsed": elapsed,
-            "percent": 100 * elapsed.days // full.days if full.days > 0 else 100,
+            "percent": percent,
             "progressbar_class": progressbar_class,
-        }
+        })
+    return context
+
 
 
 def get_type(number):
