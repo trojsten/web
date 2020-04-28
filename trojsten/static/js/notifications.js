3acb3f9e80cef79d83ec20dbc0e3c6b3e9f1fa63
Adam (ano95) <adam@ano95.eu>
2020-04-28 23:26:42 +0200

    postJSON vraj neexistuje :/

--- trojsten/static/js/notifications.js
+++ trojsten/static/js/notifications.js
@@ -26,7 +26,7 @@
       if (data.unread > 0) {
         $('#notification-box-content').append('<li role="presentation" id="notifications-read-all"><a href="#"><i class="glyphicon glyphicon-remove"></i> Označiť všetky za prečítané</a></li>')
         $("#notifications-read-all").click(() => {
-          $.postJSON(window.TROJSTEN_NOTIFY_READ_ALL_URL, update_data)
+          $.post(window.TROJSTEN_NOTIFY_READ_ALL_URL, update_data)
         })
       }
 
