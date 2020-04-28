ae3f11d33fe681ab95c75aadd58c6fc2ebc7d524
Adam (ano95) <adam@ano95.eu>
2020-04-28 23:25:06 +0200

    Add tests

--- trojsten/notifications/tests.py
+++ trojsten/notifications/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from trojsten.notifications.utils import notify
@@ -37,5 +38,22 @@ class GenericTest(TestCase):
         notify([self.user, self.user2], "foo", "bar", "https://trojsten.sk")
 
         query = Notification.objects.filter(channel="foo")
+        self.assertEquals(query.count(), 1)
+        self.assertEquals(query.get().user, self.user2)
+
+    def test_read_all(self):
+        notify([self.user], "foo", "msg", "https://trojsten.sk")
+        notify([self.user], "foo", "msg2", "https://trojsten.sk")
+        notify([self.user2], "foo", "msg2", "https://trojsten.sk")
+
+        response = self.client.post(reverse("notification_read_all"))
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEquals(Notification.objects.filter(channel="foo", was_read=False).count(), 3)
+
+        self.client.force_login(self.user)
+        response = self.client.post(reverse("notification_read_all"))
+        self.assertEqual(response.status_code, 200)
+        query = Notification.objects.filter(channel="foo", was_read=False)
         self.assertEquals(query.count(), 1)
         self.assertEquals(query.get().user, self.user2)
