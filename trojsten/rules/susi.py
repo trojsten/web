8c880ffe1fef0397254985adeb71921bce5feed7
milos7250 <milos.micik7@gmail.com>
2020-09-04 19:09:36 +0100

    Change category names.

--- trojsten/rules/susi.py
+++ trojsten/rules/susi.py
@@ -11,12 +11,12 @@ from trojsten.submit.models import Submit
 from .default import CompetitionRules
 from .default import FinishedRoundsResultsRulesMixin as FinishedRounds
 
-SUSI_ALEF = "alef"
-SUSI_BET = "bet"
-SUSI_GIMEL = "gimel"
+SUSI_AGAT = "agat"
+SUSI_BLYSKAVICA = "blyskavica"
+SUSI_CIFERSKY_CECH = "cifersky cech"
 
 
-SUSI_ALEF_MAX_COEFFICIENT = 8
+SUSI_AGAT_MAX_COEFFICIENT = 8
 SUSI_ELIGIBLE_FOR_TASK_BOUND = [8, 8, 1000, 1000, 1000, 1000, 1000]
 
 SUSI_CAMP_TYPE = "SuŠi sústredenie"
@@ -105,30 +105,30 @@ class SUSIResultsGenerator(CategoryTagKeyGeneratorMixin, ResultsGenerator):
 
     def run(self, res_request):
         self.prepare_coefficients(res_request.round)
-        res_request.has_submit_in_bet = set()
+        res_request.has_submit_in_blyskavica = set()
         for submit in Submit.objects.filter(
             task__round__semester=res_request.round.semester, task__number__in=[6, 7]
         ).select_related("user"):
-            res_request.has_submit_in_bet.add(submit.user)
+            res_request.has_submit_in_blyskavica.add(submit.user)
         return super(SUSIResultsGenerator, self).run(res_request)
 
     def is_user_active(self, request, user):
         active = super(SUSIResultsGenerator, self).is_user_active(request, user)
         coefficient = self.get_user_coefficient(user, request.round)
 
-        if self.tag.key == SUSI_ALEF:
+        if self.tag.key == SUSI_AGAT:
             active = active and (
-                (coefficient <= SUSI_ALEF_MAX_COEFFICIENT)
+                (coefficient <= SUSI_AGAT_MAX_COEFFICIENT)
                 and not self.get_graduation_status(user, request.round)
             )
 
-        if self.tag.key == SUSI_BET:
+        if self.tag.key == SUSI_BLYSKAVICA:
             active = active and (
-                (coefficient > SUSI_ALEF_MAX_COEFFICIENT or user in request.has_submit_in_bet)
+                (coefficient > SUSI_AGAT_MAX_COEFFICIENT or user in request.has_submit_in_blyskavica)
                 and not (self.get_graduation_status(user, request.round))
             )
 
-        if self.tag.key == SUSI_GIMEL:
+        if self.tag.key == SUSI_CIFERSKY_CECH:
             active = active
 
         return active
@@ -136,7 +136,7 @@ class SUSIResultsGenerator(CategoryTagKeyGeneratorMixin, ResultsGenerator):
     def deactivate_row_cells(self, request, row, cols):
         coefficient = self.get_user_coefficient(row.user, request.round)
 
-        # Count only tasks your coefficient is eligible for, ignoring category Gimel
+        # Count only tasks your coefficient is eligible for, ignoring category Cifersky Cech
         for key in row.cells_by_key:
             if SUSI_ELIGIBLE_FOR_TASK_BOUND[key] < coefficient and not self.get_graduation_status(
                 row.user, request.round
@@ -177,9 +177,9 @@ class SUSIResultsGenerator(CategoryTagKeyGeneratorMixin, ResultsGenerator):
 class SUSIRules(FinishedRounds, CompetitionRules):
 
     RESULTS_TAGS = {
-        SUSI_ALEF: ResultsTag(key=SUSI_ALEF, name="Alef"),
-        SUSI_BET: ResultsTag(key=SUSI_BET, name="Bet"),
-        SUSI_GIMEL: ResultsTag(key=SUSI_GIMEL, name="Gimel"),
+        SUSI_AGAT: ResultsTag(key=SUSI_AGAT, name="Agat"),
+        SUSI_BLYSKAVICA: ResultsTag(key=SUSI_BLYSKAVICA, name="Blyskavica"),
+        SUSI_CIFERSKY_CECH: ResultsTag(key=SUSI_CIFERSKY_CECH, name="Cifersky Cech"),
     }
 
     RESULTS_GENERATOR_CLASS = SUSIResultsGenerator
