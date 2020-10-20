5d79fab67db2964eda1879419c64bbbd8f6155ff
Adam Král <adam@xn--krl-fla.com>
2020-10-20 22:44:43 +0200

    remove visibility from html tree

--- trojsten/special/plugin_prask_7_1_1/static/plugin_prask_7_1_1/neoscreen.js
+++ trojsten/special/plugin_prask_7_1_1/static/plugin_prask_7_1_1/neoscreen.js
@@ -1,5 +1,14 @@
-hideFunction = element => element.style.visibility="hidden"
-showFunction = element => element.style.visibility="visible"
+hideFunction = element => {
+    element.style.visibility="hidden";
+    element.style.borderWidth = "0";
+}
+showFunction = element => {
+    element.style.removeProperty("visibility");
+    element.style.removeProperty("border-width");
+    if(element.style.length===0){
+        element.removeAttribute("style");
+    }
+}
 
 makeSpans = (element) => {
     element.textContent.split("").reverse().forEach((char) => {
@@ -34,16 +43,21 @@ recursivelyApplyFunction = (element, fx, timeDelta=0,depth=0) => {
         recursivelyApplyFunction(child, fx, timeDelta,depth+1);
     });
 }
-setTimeout(() =>{
-recursivelyMakeSpans(document.querySelector("#level"));
-recursivelyApplyFunction(document.querySelector("#level"),hideFunction);
-recursivelyApplyFunction(document.querySelector("#level"), showFunction,50);
-setTimeout(() =>{
-    document.querySelectorAll("span").forEach(x=>x.outerHTML=x.innerHTML)
-    document.normalize();
-},time)
-},1000)
-
+neoScreen = () => {
+    setTimeout(() => {
+        recursivelyMakeSpans(document.querySelector("#level"));
+        recursivelyApplyFunction(document.querySelector("#level"), hideFunction);
+        setTimeout(() => {
+            document.body.classList.add("visible");
+        }, 10)
+        recursivelyApplyFunction(document.querySelector("#level"), showFunction, 50);
+        setTimeout(() => {
+            document.querySelectorAll("span").forEach(x => x.outerHTML = x.innerHTML)
+            document.normalize();
+        }, time)
+    }, 1000)
+}
+neoScreen();
 //document.querySelectorAll("span").forEach(span=> span.style.visibility = "visible");
 
 //TODO: pexeso
