603f5f4d4772f9f4ab86860e266cdb5bb7bbf9a4
Adam Zahradník <adam@ano95.eu>
2021-01-11 11:44:09 +0100

    Fix sidebar. (#1360)

--- trojsten/static/js/menu.js
+++ trojsten/static/js/menu.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
     $("body").on( "swiperight", function( event ) {
         if(event.swipestart.coords[0] < $('body').width() * .1) {
-            $('.nav-menu:not(.offcanvas-clone):not(:visible)').offcanvas('show');
+            $('.row-offcanvas').addClass('active')
         }
     });
 
     $("body").on( "swipeleft", function( event ) {
-        $('.nav-menu.in:not(.offcanvas-clone)').offcanvas('hide');
+        $('.row-offcanvas').removeClass('active')
     });
 
-    $(".offcanvas-xs").on("show.bs.offcanvas", function(event) {
-        $(".navbar-toggle").addClass("active");
-    });
-
-    $(".offcanvas-xs").on("hidden.bs.offcanvas", function(event) {
-        $(".navbar-toggle").removeClass("active");
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
     });
 });
