e770efa101ba7a2541bff60e28d068d3971f6b40
sobkulir <r.sobkuliak@gmail.com>
2021-04-16 01:24:22 +0200

    Add GET params text.

--- trojsten/special/plugin_prask_7_2_1/urls.py
+++ trojsten/special/plugin_prask_7_2_1/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 
 from trojsten.special.plugin_prask_7_2_1 import views
 
@@ -11,6 +11,11 @@ urlpatterns = [
     url(r"^$", RedirectView.as_view(url="intro/", permanent=True)),
     url(r"^intro/$", views.intro),
     url(r"^get_hint/(?P<level>\d+)/$", views.get_hint, name="get_hint"),
+    url(
+        r"^get_params_text/$",
+        TemplateView.as_view(template_name="plugin_prask_7_2_1/get_params.html"),
+        name="get_params",
+    ),
 ] + [
     url(r"^%s/$" % lvl.url, views.level, {"level": id, "source_fname": lvl.source_fname})
     for (id, lvl) in enumerate(LEVELS)
