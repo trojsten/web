564634fe6f24ae4f46764ccdcce8cf4e4f43bf1f
sobkulir <r.sobkuliak@gmail.com>
2021-04-22 10:21:51 +0200

    Add hint for frozen level and fix poopup.

--- trojsten/special/plugin_prask_7_2_1/urls.py
+++ trojsten/special/plugin_prask_7_2_1/urls.py
@@ -16,6 +16,10 @@ urlpatterns = [
         TemplateView.as_view(template_name="plugin_prask_7_2_1/get_params.html"),
         name="get_params",
     ),
+    url(
+        r"^(frozen|ladovekralovstvo|ladove_kralovstvo)/$",
+        TemplateView.as_view(template_name="plugin_prask_7_2_1/frozen.html"),
+    ),
 ] + [
     url(r"^%s/$" % lvl.url, views.level, {"level": id, "source_fname": lvl.source_fname})
     for (id, lvl) in enumerate(LEVELS)
