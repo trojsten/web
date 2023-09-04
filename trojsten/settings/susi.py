8f14fe9851fe7ae130a8b1e11558246c2e1fe068
Adam Zahradník <adam@zahradnik.xyz>
2023-09-04 21:10:14 +0200

    susi-submit-extensions: add .jpeg

--- trojsten/settings/susi.py
+++ trojsten/settings/susi.py
@@ -3,7 +3,7 @@ from trojsten.settings.production import *
 SITE_ID = 11
 NAVBAR_SITES = [4, 1, 3, 5]
 
-SUBMIT_DESCRIPTION_ALLOWED_EXTENSIONS += [".jpg", ".png", ".pptx", ".gif", ".webp", ".avif"]
+SUBMIT_DESCRIPTION_ALLOWED_EXTENSIONS += [".jpg", ".jpeg", ".png", ".pptx", ".gif", ".webp", ".avif"]
 SUBMIT_DESCRIPTION_ALLOWED_MIMETYPES += [
     "image/jpeg",
     "image/png",
