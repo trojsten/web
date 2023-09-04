808285cc39046401565d80f6b34f41972394d3d9
Matej Novota <matej.novota@gmail.com>
2023-09-04 21:10:49 +0200

    Add allowed formats for suši submits (#1427)
    * Add allowed formats for suši submits
    
    * new line at end of file
    
    * susi-submit-extensions: add .jpeg
    
    ---------
    
    Co-authored-by: Michal Stanik <michal.stanik00@gmail.com>
    Co-authored-by: Adam Zahradník <adam@zahradnik.xyz>

--- trojsten/settings/susi.py
+++ trojsten/settings/susi.py
@@ -2,3 +2,13 @@ from trojsten.settings.production import *
 
 SITE_ID = 11
 NAVBAR_SITES = [4, 1, 3, 5]
+
+SUBMIT_DESCRIPTION_ALLOWED_EXTENSIONS += [".jpg", ".jpeg", ".png", ".pptx", ".gif", ".webp", ".avif"]
+SUBMIT_DESCRIPTION_ALLOWED_MIMETYPES += [
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "image/webp",
+    "image/avif",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+]
